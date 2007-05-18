30ee230b27e8760cc56d22302f26ebcf346b1d7d
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2007-05-18 16:47:40 +0000

    Apply UIUC patches to fix MySQL specific find_by_sql statements.  BibApp now working on MySQL and PostgreSQL.
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@18 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,58 +3,78 @@ class SearchController < ApplicationController
   def index
     if params[:q]
       @query = params[:q]
+      @query.downcase
       @query = "%" + @query + "%"
       
-      @people = Person.find_by_sql([
-        "select * from people
-        where first_last like ?
-        order by first_name", @query])
+      @people = Person.find(:all,
+                :conditions=>["LOWER(first_name) LIKE ? OR
+                               LOWER(last_name) LIKE ?", @query, @query],
+                :order=>"first_name")
 
-      @groups = Group.find_by_sql([
-        "select * from groups
-        where name like ?
-        order by name", @query])
+      @groups = Group.find(:all,
+                :conditions=>["LOWER(name) LIKE ?", @query],
+                :order=>"name")
     end
   end
   
   def experts
     @query = params['query']
     @prep_query = "%" + @query + "%"
-    @people_results = Person.find_by_sql([  
-      "Select p.*, count(c.id) as papers, max(c.pub_year) as recency
-      From citations c
-      join authorships au on c.id = au.citation_id
-      join people p on au.person_id = p.id
-      where citation_state_id = 3
-      and c.keywords like ?
-      or c.abstract like ?
-      or c.title_primary like ?
-      group by p.id
-      order by max(pub_year) DESC, count(c.id) DESC
-      limit 10", @prep_query, @prep_query, @prep_query])
+
+    # In order to search tags, we need a version of 
+    # the query where we've replaced all spaces with '-'
+    @prep_tag_query = @prep_query.gsub(/\s+/, '-')
+    
+    #This query should work in both MySQL and Postgres
+    @people_results = Person.find_by_sql([
+      "Select per.*, cit.papers, cit.recency
+      From people per
+      JOIN (SELECT p.id as person_id, count(c.id) as papers, max(c.pub_year) as recency 
+            From citations c 
+              join authorships au on c.id = au.citation_id 
+              join people p on au.person_id = p.id
+              join taggings tg on c.id=tg.taggable_id
+              join tags t on tg.tag_id=t.id
+            where citation_state_id = 3 
+            and t.name like ? 
+            or c.abstract like ? 
+            or c.title_primary like ?
+            group by p.id) as cit
+      ON per.id=cit.person_id
+      order by cit.recency DESC, cit.papers DESC
+      limit 10", @prep_tag_query, @prep_query, @prep_query])
       
-    @group_results = Group.find_by_sql([
-      "select g.*, count(c.id) as papers, max(c.pub_year) as recency
-      from citations c
-      join authorships au on c.id = au.citation_id
-      join people p on au.person_id = p.id
-      join memberships m on m.person_id = p.id
-      join groups g on m.group_id = g.id
-      where citation_state_id = 3
-      and c.title_primary like ?
-      or c.abstract like ?
-      or c.keywords like ?
-      group by g.id
-      order by count(c.id) DESC
-      limit 5", @prep_query, @prep_query, @prep_query])
+      #This query should work in both MySQL and Postgres
+      @group_results = Group.find_by_sql([
+      "Select grp.*, cit.papers, cit.recency
+        From groups grp
+        JOIN (select g.id as group_id, count(c.id) as papers, max(c.pub_year) as recency
+              from citations c
+                join authorships au on c.id = au.citation_id
+                join people p on au.person_id = p.id
+                join memberships m on m.person_id = p.id
+                join groups g on m.group_id = g.id
+                join taggings tg on c.id=tg.taggable_id
+                join tags t on tg.tag_id=t.id
+              where citation_state_id = 3
+              and t.name like ? 
+              or c.abstract like ? 
+              or c.title_primary like ?
+              group by g.id) as cit
+      ON grp.id=cit.group_id 
+      order by cit.papers DESC
+      limit 5", @prep_tag_query, @prep_query, @prep_query])
   end
   
   def results
     @query = params[:query]
-    @results = Person.find_by_sql([
-      "select * from people
-      where last_name like ?
-      or first_name like ?
-      order by last_name, first_name", @query, @query])
+    @query.downcase
+    @query = "%" + @query + "%"
+    
+    
+    @results = Person.find(:all,
+        :conditions=>["LOWER(first_name) LIKE ? OR
+                       LOWER(last_name) LIKE ?", @query, @query],
+        :order=>"last_name, first_name")
   end
 end
