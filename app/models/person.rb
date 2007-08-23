197a35464bf825e8d43e7b4bb8b44d6291e32285
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2007-08-23 16:32:33 +0000

    Fix for Issue#21: Empty "publication names" are sometimes listed in a person's "Popular publications" listing

--- app/models/person.rb
+++ app/models/person.rb
@@ -94,6 +94,7 @@ class Person < ActiveRecord::Base
       left join publishers pub on publ.publisher_id = pub.id
       where au.person_id = ?
       and length(c.issn_isbn) > 0
+	  and length(c.periodical_full) > 0
       and c.citation_state_id = 3
       group by c.issn_isbn, c.periodical_full, c.title_tertiary 
       order by count DESC, c.periodical_full
