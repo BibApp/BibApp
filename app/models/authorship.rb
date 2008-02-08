62fecfd8e4ac1610dafb8f877e9c6e7876a9f66f
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-02-08 22:51:18 +0000

    Feature: Add rich Authorships and PenName callbacks to appropriately set/destroy Authorship rows.

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,22 +1,8 @@
 class Authorship   < ActiveRecord::Base
   belongs_to :person
   belongs_to :citation
+  belongs_to :pen_name
   
-  validates_presence_of :person_id, :citation_id
+  validates_presence_of :person_id, :citation_id, :pen_name_id
   validates_uniqueness_of :citation_id, :scope => :person_id
-  
-  class << self
-    def create_batch!(person, citation_data)
-      cites = Citation.import_batch!(citation_data)
-      create_batch_from_citations!(person, cites)
-      return cites
-    end
-  
-    def create_batch_from_citations!(person, citations)
-      return if not citations.respond_to? :each
-      citations.each do |c|
-        Authorship.create(:person_id => person.id, :citation_id => c.id )
-      end
-    end
-  end
 end
