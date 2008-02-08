8f9c689665a460649f5ebb74f1fc4397c9f247a2
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-02-08 22:46:10 +0000

    Bug fix: Load frozen gem libs
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@214 6a0f952e-c525-0410-88e5-87db5789ed7d

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,10 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   config.load_paths += %W( #{RAILS_ROOT}/app/models/citation_subclasses )
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end
+
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
