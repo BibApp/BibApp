afb7841126e33e993563b86a67d29e96cd5de4d6
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-03-18 22:05:48 +0000

    Authorship Algorithm: Initial pass at calculating score.  Super fun!
    To use:
    
    * Add a person
    * Import some citations for person
    * Give that person some PenNames
    * Look at the Authorships table
    
    Todo:
    
    * Rename Authorships to Contributorships
    * Pass Contributorship.type (ex. "Author", "Editor")
    * Add Person methods to look for :
    - Person.authorships
    - Person.editorships
    
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@337 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -5,4 +5,74 @@ class Authorship   < ActiveRecord::Base
   
   validates_presence_of :person_id, :citation_id, :pen_name_id
   validates_uniqueness_of :citation_id, :scope => :person_id
+  
+  before_validation_on_create :set_initial_states
+  after_create :calculate_score
+
+  def calculate_score
+    # Build the calcuated "score", a rough guess at who has written a citation
+    #
+    # Field           Value   Scoring Algorithm
+    # ---------------------------------------------
+    # Yearspan        25      If matches = 25 pts
+    # Publication     25      If matches = 25 pts
+    # Collaborators   25      (25/total) * matching
+    # Keywords        25      (25/total) * matching
+    
+    # @TODO:
+    # Done: Hash of validated person field values
+         
+    # All scores begin at zero!
+    logger.debug("\nPerson: #{self.person.first_last}")
+    
+    scoring_hash = self.person.scoring_hash
+    
+    # Add year value if present
+    year_score = 0
+    year_score = 25 if scoring_hash[:years].include?(self.citation.year)
+          
+    # Add publication value if present
+    publication_score = 0
+    publication_score = 25 if scoring_hash[:publication_ids].include?(self.citation.publication.id)
+    
+    # Add collaborators value if present
+    col_poss = self.citation.name_strings.size
+    col_matches = 0
+
+    self.citation.name_strings.each do |ns|
+      col_matches = (col_matches + 1) if scoring_hash[:collaborator_ids].include?(ns.id)
+    end
+    
+    collaborator_score = 0
+    collaborator_score = ((25/col_poss)*col_matches)
+    
+    # Add keywords value if present
+    key_poss = self.citation.keywords.size
+    key_matches = 0
+    
+    self.citation.keywords.each do |k|
+      key_matches = (key_matches + 1) if scoring_hash[:keyword_ids].include?(k.id)
+    end
+    
+    keyword_score = 0
+    keyword_score = ((25/key_poss)*key_matches)
+    
+    # Debugging the scoring algoritm
+    logger.debug("\n\n========================================")
+    logger.debug("Year: #{year_score}")
+    logger.debug("Publication: #{publication_score}")
+    logger.debug("Collaborators: (25/#{col_poss}) * #{col_matches} = #{collaborator_score}")
+    logger.debug("Keywords: (25/#{key_poss}) * #{key_matches} = #{keyword_score}")
+    logger.debug("*Final Score:* #{(year_score + publication_score + collaborator_score + keyword_score)}")
+    logger.debug("========================================\n\n")
+
+    self.score = (year_score + publication_score + collaborator_score + keyword_score)
+    self.save
+  end
+  
+  # All Citations begin unverified
+  def set_initial_states
+    self.authorship_state_id = 1
+    self.score = 0
+  end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -28,4 +28,44 @@ class Person < ActiveRecord::Base
     # TODO: do this right. The vector subtraction is dumb.
     return all_name_strings - name_strings
   end
+  
+  # Person Contributorship Calculation Fields
+  def verified_publications
+    Authorship.find_all_by_person_id_and_authorship_state_id(self.id,2)
+  end
+  
+  def known_years
+    # Build an array of verified publication year strings
+    # Ex. ["2001,2002,..."]
+    self.verified_publications.collect{|vp| vp.citation.year}.uniq
+  end
+  
+  def known_publication_ids
+    # Build an array of verified publication objects
+    # Ex. [#<Publication id: 1...>,#<Publication id: 2...>,..."]
+    self.verified_publications.collect{|vp| vp.citation.publication.id}.uniq
+  end
+  
+  def known_collaborator_ids
+    # Build an array of verified name_string objects
+    # Ex. [#<NameString id: 1...>,#<NameString id: 2...>,..."]    
+    self.verified_publications.collect{|vp| vp.citation.name_strings.collect{|ns| ns.id}}.flatten.uniq
+  end
+  
+  def known_keyword_ids
+    # Build an array of verified keyword objects
+    # Ex. [#<Keyword id: 1...>,#<Keyword id: 2...>,..."]
+    self.verified_publications.collect{|vp| vp.citation.keywords.collect{|k| k.id}}.flatten.uniq
+  end
+  
+  def scoring_hash
+    # Return a hash comprising all the Contributorship scoring methods
+    scoring_hash = {
+      :years => self.known_years, 
+      :publication_ids => self.known_publication_ids,
+      :collaborator_ids => self.known_collaborator_ids,
+      :keyword_ids => self.known_keyword_ids
+    }
+    scoring_hash
+  end
 end
