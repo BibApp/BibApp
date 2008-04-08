bc2ab3e762bd00f6fc8b90d1477415c1844d6137
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-04-08 18:16:29 +0000

    Group view: @citations to paginate should be based upon Contributorships not Citations.  Fixed.
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@390 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,14 +16,12 @@ class GroupsController < ApplicationController
       @citations = Citation.paginate(
         :all,
         :joins => "
-          join citation_name_strings on citations.id = citation_name_strings.citation_id
-          join name_strings on citation_name_strings.name_string_id = name_strings.id
-          join pen_names on name_strings.id = pen_names.name_string_id
-          join people on pen_names.person_id = people.id
+          join contributorships on citations.id = contributorships.citation_id
+          join people on contributorships.person_id = people.id
           join memberships on people.id = memberships.person_id
           join groups on memberships.group_id = groups.id
           ",
-        :conditions => ["groups.id = ? and citations.citation_state_id = ?", params[:id], 3],
+        :conditions => ["groups.id = ? and contributorships.contributorship_state_id = ?", params[:id], 2],
         :order => "citations.year DESC, citations.title_primary",
         :page => params[:page] || 1,
         :per_page => 10
