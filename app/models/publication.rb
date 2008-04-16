38116d656f5450785d0c43b2e4540245d5a06014
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-04-16 20:50:45 +0000

    Bug Fixes:  The alpha-pagination queries for People, Publishers and Publications were erroring out in PostgreSQL.  Fixed them so that they *should* work fine for both MySQL and PostgreSQL.  Also added links to get to People/Groups lists from the citation index.html.haml
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@426 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -61,12 +61,12 @@ class Publication < ActiveRecord::Base
   
   class << self
 
+    # return the first letter of each name, ordered alphabetically
     def letters
       find(
         :all,
-        :select => 'SUBSTR(name, 1, 1) AS letter',
-        :group  => 'SUBSTR(name, 1, 1)',
-        :order  => 'name'
+        :select => 'DISTINCT SUBSTR(name, 1, 1) AS letter',
+        :order  => 'letter'
       )
     end
   
