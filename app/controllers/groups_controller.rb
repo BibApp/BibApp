775a6dbf715607aa41a7078cb2d743a9654634e7
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-05-06 17:13:23 +0000

    Cleanup: Group Controller
    * Alpha pagination for Groups
    * Group/index view fixed name sort ordering
    * Group/show view tied more closely to Solr
    * Group/show view div re-architecture

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,22 +3,23 @@ class GroupsController < ApplicationController
     build :all
 
     before :index do
-      @groups = Group.paginate(
-        :all,
-        :order => "name",
-        :page => params[:page] || 1,
-        :per_page => 10
+      @a_to_z = Group.letters.collect { |g| g.letter }
+      @page = params[:page] || @a_to_z[0]
+      @current_objects = Group.find(
+        :all, 
+        :conditions => ["name like ?", "#{@page}%"], 
+        :order => "name"
       )
     end
     
-    before :show do 
+    before :show do
       @query = @current_object.solr_id
+      @sort = params[:sort] || "year desc"
+      @fetch = @query + ";" + @sort
       @filter = params[:fq] || ""
       @filter = @filter.split("+>+").each{|f| f.strip!}
-      @q,@docs,@facets = Index.fetch(@query, @filter)
-      
-      logger.debug("\n\n Facets: #{@facets.inspect}\n\n")
-      
+      @q,@docs,@facets = Index.fetch(@fetch, @filter, @sort)
+
       @title = @current_object.name
     end
   end
