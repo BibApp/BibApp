7a99e8f53b517576c3038eeeef121e11c16300c2
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-05-06 16:53:28 +0000

    Feature add: Contributorships/archivable view.
    *Warning*: personalize.rb.example modified to allow localization of Sherpa color descriptions.  Please update your personalize.rb file (view will break otherwise).

--- app/controllers/contributorships_controller.rb
+++ app/controllers/contributorships_controller.rb
@@ -13,6 +13,24 @@ class ContributorshipsController < ApplicationController
     end
   end
   
+  def admin
+    @contributorship = Contributorship.find(
+      :first, 
+      :conditions => ["contributorship_state_id = ? and hide = ?", 1, false],
+      :order => "citation_id"
+    )
+    
+    if @contributorship == nil
+      # Do Nothing
+    else
+      @claims = Contributorship.find(
+        :all, 
+        :conditions => ["citation_id = ? and contributorship_state_id = ? and hide = ?", @contributorship.citation_id, 1, false],
+        :order => "score desc"
+      )
+    end
+  end
+  
   def verify
     # @TODO: Auth check here
     @contributorship = Contributorship.find(params[:id])
@@ -28,7 +46,8 @@ class ContributorshipsController < ApplicationController
     @contributorships = person.contributorships.to_show
     
     respond_to do |format|
-      format.js { render :action => :verify_contributorship }
+      format.html { redirect_to :action => :admin }
+      format.js   { render :action => :verify_contributorship }
     end
   end
   
@@ -46,7 +65,42 @@ class ContributorshipsController < ApplicationController
     
     # RJS action removes the denied citation from the view
     respond_to do |format|
-      format.js { render :action => :deny_contributorship }
+      format.html { redirect_to :action => :admin }
+      format.js   { render :action => :deny_contributorship }
     end
+  end
+  
+  def archivable
+    # Find Person for view
+    @person = Person.find(params[:person_id])
+    
+    # Collect data for Sherpa color table
+    @pub_table = Contributorship.find_by_sql(
+      ["select count(ctrb.id) as count, pub.romeo_color as color
+      from contributorships ctrb
+      join citations c on ctrb.citation_id = c.id
+      join people p on ctrb.person_id = p.id
+      join publishers pub on c.publisher_id = pub.id
+      where p.id = ?
+      group by pub.romeo_color
+      order by pub.romeo_color", @person.id]
+    )
+    
+    # Calculate the sum of each Sherpa color
+    @pub_totals = @pub_table.collect{|c| c.count}.inject{|sum, n| sum.to_i + n.to_i}
+    
+    # Collect data for Publication table
+    @publ_table = Contributorship.find_by_sql(
+        ["select count(ctrb.id) as count, publ.name as name, pub.name as pub_name, pub.romeo_color as color
+        from contributorships ctrb
+        join citations c on ctrb.citation_id = c.id
+        join people p on ctrb.person_id = p.id
+        join publications publ on c.publication_id = publ.id
+        join publishers pub on c.publisher_id = pub.id
+        where p.id = ?
+        group by publ.name
+        order by count(ctrb.id) desc", @person.id]
+      )
+    
   end
 end
