0ed83777c371dbaaec217338a5373a4981341b5d
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-05-21 16:18:17 +0000

    SOLR Port fix:  You can now use the solr.yml file to specify which port you want Solr to run on for BibApp.  This hadn't worked properly since we got rid of acts_as_solr.   By default, it still uses port 8983 for Solr.
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@513 6a0f952e-c525-0410-88e5-87db5789ed7d

--- config/initializers/solr.rb
+++ config/initializers/solr.rb
@@ -1,8 +1,16 @@
 # Solr Path (in /vendor/solr)
 SOLR_PATH = "#{File.dirname(__FILE__)}/../../vendor/solr" unless defined? SOLR_PATH
 
-# Solr Port
+# Solr Config (in /config/solr.yml)
+SOLR_CONFIG = "#{File.dirname(__FILE__)}/../../config/solr.yml" unless defined? SOLR_CONFIG
+
+# Load our Solr Configuration (from solr.yml) for our current environment
+SOLR_SETTINGS = YAML::load(File.read(SOLR_CONFIG))[RAILS_ENV]
+
+# Set Solr port (default to port 8983 if no port found in solr.yml)
+# (This SOLR_PORT variable is used by /lib/tasks/solr.rake when starting Solr)
+SOLR_PORT = SOLR_SETTINGS['port'] if SOLR_SETTINGS and SOLR_SETTINGS['port']
 SOLR_PORT = "8983" unless defined? SOLR_PORT
 
-# Solr Connection
+# Solr Connection (used by /app/models/index.rb)
 SOLRCONN = Solr::Connection.new("http://localhost:#{SOLR_PORT}/solr")
