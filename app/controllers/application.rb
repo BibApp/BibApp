933e381c8e497fdeed220a3315c7821229ad527d
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-05-29 21:45:59 +0000

    Basic Authentication:  MAJOR FEATURE!  WARNING: You'll need to run 'rake db:migrate' and also update 'personalize.rb', and create your own custom smtp.yml configuration file (which sends activation emails).
    * Added 'restful_authentication' plugin
    * Set it up so that 'signup' requires activation (an activation email is sent to user, based on SMTP settings in smtp.yml)
    * Added custom 'passwords' Controller/View to allow for changing password and requesting a new password
    
    Everything *should* be working, but I'm going to pull down to our linux test instance to be sure the Email activation is being sent properly, etc.
    
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@573 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,9 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  # Include AuthenticationSystem so all controllers support authentication
+  include AuthenticatedSystem
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '6ef4f4bba39aae6ef1a1da02e1ace6d8'
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+# This controller handles the login/logout function of the site.  
+class SessionsController < ApplicationController
+  
+  # load new.html.haml
+  def new
+  end
+
+  # Create - Login a user into the system
+  def create
+    self.current_user = User.authenticate(params[:login], params[:password])
+    if logged_in?
+      if params[:remember_me] == "1"
+        current_user.remember_me unless current_user.remember_token?
+        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+      end
+      redirect_back_or_default('/')
+      flash[:notice] = "Logged in successfully"
+    else
+      flash[:notice] = "Username or password was invalid."
+      render :action => 'new'
+    end
+  end
+
+  # Destroy - Logout a user from the system
+  def destroy
+    self.current_user.forget_me if logged_in?
+    cookies.delete :auth_token
+    reset_session
+    flash[:notice] = "You have been logged out."
+    redirect_back_or_default('/')
+  end
+end
