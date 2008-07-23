c4eedf516e8607135f85d43bed0ee523f89af48f
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-07-23 17:11:04 +0000

    Rails 2.1 Upgrade - Last commit....There were a few extra configs and scripts that didn't make it in properly.
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@762 6a0f952e-c525-0410-88e5-87db5789ed7d

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 45) do
+ActiveRecord::Schema.define(:version => 46) do
 
   create_table "attachments", :force => true do |t|
     t.string   "filename"
@@ -179,7 +179,7 @@ ActiveRecord::Schema.define(:version => 45) do
     t.datetime "updated_at"
   end
 
-  add_index "pen_names", ["person_id", "name_string_id"], :name => "author_person_join", :unique => true
+  add_index "pen_names", ["name_string_id", "person_id"], :name => "author_person_join", :unique => true
 
   create_table "people", :force => true do |t|
     t.integer  "external_id"
@@ -267,7 +267,7 @@ ActiveRecord::Schema.define(:version => 45) do
   end
 
   add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_type", "taggable_id"], :name => "index_taggings_on_taggable_id_and_taggable_type"
+  add_index "taggings", ["taggable_id", "taggable_type"], :name => "index_taggings_on_taggable_id_and_taggable_type"
 
   create_table "tags", :force => true do |t|
     t.string "name"
