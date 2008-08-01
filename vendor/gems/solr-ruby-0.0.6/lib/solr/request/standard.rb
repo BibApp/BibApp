5c80c787efbfbd418c2c6d5cdb4cf441fb92823b
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-08-01 20:16:29 +0000

    Gem Dependencies:  Refreezing 'htmlentities', 'namecase' and 'solr-ruby'.  For some reason these three are not working with the 'rake gems' command.  Small updates to environment.   YOU MUST RUN 'rake gems:install' to install all required gems!!

---
+++ vendor/gems/solr-ruby-0.0.6/lib/solr/request/standard.rb
@@ -0,0 +1,135 @@
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class Solr::Request::Standard < Solr::Request::Select
+
+  VALID_PARAMS = [:query, :sort, :default_field, :operator, :start, :rows, :shards,
+    :filter_queries, :field_list, :debug_query, :explain_other, :facets, :highlighting, :mlt]
+  
+  def initialize(params)
+    super('standard')
+    
+    raise "Invalid parameters: #{(params.keys - VALID_PARAMS).join(',')}" unless 
+      (params.keys - VALID_PARAMS).empty?
+    
+    raise ":query parameter required" unless params[:query]
+    
+    @params = params.dup
+    
+    # Validate operator
+    if params[:operator]
+      raise "Only :and/:or operators allowed" unless 
+        [:and, :or].include?(params[:operator])
+        
+      @params[:operator] = params[:operator].to_s.upcase
+    end
+
+    # Validate start, rows can be transformed to ints
+    @params[:start] = params[:start].to_i if params[:start]
+    @params[:rows] = params[:rows].to_i if params[:rows]
+    
+    @params[:field_list] ||= ["*","score"]
+    
+    @params[:shards] ||= []
+  end
+  
+  def to_hash
+    hash = {}
+    
+    # standard request param processing
+    sort = @params[:sort].collect do |sort|
+      key = sort.keys[0]
+      "#{key.to_s} #{sort[key] == :descending ? 'desc' : 'asc'}"
+    end.join(',') if @params[:sort]
+    hash[:q] = sort ? "#{@params[:query]};#{sort}" : @params[:query]
+    hash["q.op"] = @params[:operator]
+    hash[:df] = @params[:default_field]
+
+    # common parameter processing
+    hash[:start] = @params[:start]
+    hash[:rows] = @params[:rows]
+    hash[:fq] = @params[:filter_queries]
+    hash[:fl] = @params[:field_list].join(',')
+    hash[:debugQuery] = @params[:debug_query]
+    hash[:explainOther] = @params[:explain_other]
+    hash[:shards] = @params[:shards].join(',') unless @params[:shards].empty?
+    
+    # facet parameter processing
+    if @params[:facets]
+      # TODO need validation of all that is under the :facets Hash too
+      hash[:facet] = true
+      hash["facet.field"] = []
+      hash["facet.query"] = @params[:facets][:queries]
+      hash["facet.sort"] = (@params[:facets][:sort] == :count) if @params[:facets][:sort]
+      hash["facet.limit"] = @params[:facets][:limit]
+      hash["facet.missing"] = @params[:facets][:missing]
+      hash["facet.mincount"] = @params[:facets][:mincount]
+      hash["facet.prefix"] = @params[:facets][:prefix]
+      hash["facet.offset"] = @params[:facets][:offset]
+      if @params[:facets][:fields]  # facet fields are optional (could be facet.query only)
+        @params[:facets][:fields].each do |f|
+          if f.kind_of? Hash
+            key = f.keys[0]
+            value = f[key]
+            hash["facet.field"] << key
+            hash["f.#{key}.facet.sort"] = (value[:sort] == :count) if value[:sort]
+            hash["f.#{key}.facet.limit"] = value[:limit]
+            hash["f.#{key}.facet.missing"] = value[:missing]
+            hash["f.#{key}.facet.mincount"] = value[:mincount]
+            hash["f.#{key}.facet.prefix"] = value[:prefix]
+            hash["f.#{key}.facet.offset"] = value[:offset]
+          else
+            hash["facet.field"] << f
+          end
+        end
+      end
+    end
+    
+    # highlighting parameter processing - http://wiki.apache.org/solr/HighlightingParameters
+    #TODO need to add per-field overriding to snippets, fragsize, requiredFieldMatch, formatting, and simple.pre/post
+    if @params[:highlighting]
+      hash[:hl] = true
+      hash["hl.fl"] = @params[:highlighting][:field_list].join(',') if @params[:highlighting][:field_list]
+      hash["hl.snippets"] = @params[:highlighting][:max_snippets]
+      hash["hl.requireFieldMatch"] = @params[:highlighting][:require_field_match]
+      hash["hl.simple.pre"] = @params[:highlighting][:prefix]
+      hash["hl.simple.post"] = @params[:highlighting][:suffix]
+      hash["hl.fragsize"] = @params[:highlighting][:fragment_size]
+      if @params[:highlighting][:alternate_fields]
+        @params[:highlighting][:alternate_fields].each do |k,v|
+          hash["f.#{k}.hl.alternateField"] = v
+        end
+      end
+      if @params[:highlighting][:max_alternate_field_length]
+        @params[:highlighting][:max_alternate_field_length].each do |k,v|
+          hash["f.#{k}.hl.maxAlternateFieldLength"] = v
+        end
+      end
+    end
+    
+    if @params[:mlt]
+      hash[:mlt] = true
+      hash["mlt.count"] = @params[:mlt][:count]
+      hash["mlt.fl"] = @params[:mlt][:field_list].join(',')
+      hash["mlt.mintf"] = @params[:mlt][:min_term_freq]
+      hash["mlt.mindf"] = @params[:mlt][:min_doc_freq]
+      hash["mlt.minwl"] = @params[:mlt][:min_word_length]
+      hash["mlt.maxwl"] = @params[:mlt][:max_word_length]
+      hash["mlt.maxqt"] = @params[:mlt][:max_query_terms]
+      hash["mlt.maxntp"] = @params[:mlt][:max_tokens_parsed]
+      hash["mlt.boost"] = @params[:mlt][:boost]
+    end
+    
+    hash.merge(super.to_hash)
+  end
+
+end
