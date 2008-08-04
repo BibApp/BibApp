9f195c9b5628b0a6adc433855910a85275832c89
tim.donohue <tim.donohue@6a0f952e-c525-0410-88e5-87db5789ed7d>
2008-08-04 15:56:36 +0000

    Dependency:  Removing HTMLEntities from /vendor/gems/    It now works as a Gem Dependency, after updating Citation Parsers and Application Helper to only include 'htmlentities' when HTMLEntities is defined, as stated in this blog:  http://www.webficient.com/2008/7/11/rails-gem-dependencies-and-plugin-errors
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@790 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   require 'config/personalize.rb'
-  require 'htmlentities'
+  require 'htmlentities' if defined? HTMLEntities
   
   def ajax_checkbox_toggle(model, person, selected)
     person = Person.find(person.id)
