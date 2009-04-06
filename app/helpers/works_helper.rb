488aad1777499a9d3c69b68b89af87edf0b3743e
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-04-06 21:32:35 +0000

    Work show view: this view was ready for considerable polish.  Added helpers and partials to better render the view.

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,2 +1,16 @@
 module WorksHelper
+  def link_to_work_authors(work, limit= nil)
+    if !work.name_strings.author.empty?
+      authors = Array.new
+      work.name_strings.author.each do |a|
+        authors << "#{link_to a.name.gsub(",", ", "), name_string_path(a)}"
+      end
+      if limit
+        if authors.size > 5
+          authors = authors.first(6) << "et al."
+        end
+      end
+      authors.join(", ")
+    end
+  end
 end
