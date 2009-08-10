aa4411e6f7a5f8548a655dfdcb4bd38c73d99ed5
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-10 20:25:49 +0000

    Background processing: removing workling and spawn.  Going forward with delayed_job for BibApp 1.0.

--- vendor/plugins/workling/lib/workling.rb
+++
@@ -1,7 +0,0 @@
-module Workling
-  class WorklingError < StandardError; end
-  class WorklingNotFoundError < WorklingError; end
-
-  mattr_accessor :load_path
-  @@load_path = File.expand_path(File.join(File.dirname(__FILE__), '../../../../app/workers')) 
-end
