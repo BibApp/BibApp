86f4c8b5568c7a3b4e29752912ff6fafaa04c11c
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-14 14:02:49 +0000

    Bug fix -- issue #74: Admin Publisher Authorities works again.

--- app/models/publisher_observer.rb
+++ app/models/publisher_observer.rb
@@ -1,9 +1,8 @@
 class PublisherObserver < ActiveRecord::Observer
  
   # Anytime a Publisher is saved (during create or update)
-  def after_save(pub)
-    pub.update_authorities
-    pub.update_machine_name
+  def after_save(publisher)
+    publisher.update_authorities
+    publisher.update_machine_name
   end
-    
 end
