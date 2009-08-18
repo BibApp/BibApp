06b0cce9b5e59e1157bbe2b1a7185dde3068bfb9
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-18 16:34:39 +0000

    COinS: adding work.subklass level openurl kev mappings.  In the long run... we'll want to expose RDF data instead of coins, which don't support common data elements like: editors!
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@1183 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/models/work_subclasses/book_whole.rb
+++ app/models/work_subclasses/book_whole.rb
@@ -6,5 +6,16 @@ class BookWhole < Work
       ['Author', 'Editor', 'Translator', 'Illustrator']
     end
   end
-
+  
+  def open_url_kevs
+    open_url_kevs = Hash.new
+    open_url_kevs[:format]      = "&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook"
+    open_url_kevs[:genre]       = "&rft.genre=book"
+    open_url_kevs[:title]       = "&rft.btitle=#{CGI.escape(self.title_primary)}"
+    open_url_kevs[:publisher]   = "&rft.pub=#{self.publisher.authority.name}"
+    open_url_kevs[:isbn]        = "&rft.isbn=#{self.publication.isbns.first[:name]}" if !self.publication.isbns.empty?
+    open_url_kevs[:date]        = "&rft.date=#{self.publication_date}"
+    
+    return open_url_kevs
+  end
 end
