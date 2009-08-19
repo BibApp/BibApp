82abc6ca8ce09f91b5c0ebd742d04dc8e76630aa
waingram <waingram@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-19 15:22:19 +0000

    DB Migrate
    Makes changes to Work subtypes
    BookEdited => BookWhole
    JournalEdited => JournalWhole
    SoundRecording => RecordingSound
    Video => RecordingMovingImage
    
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@1195 6a0f952e-c525-0410-88e5-87db5789ed7d

---
+++ db/migrate/20090814194316_update_work_types.rb
@@ -0,0 +1,36 @@
+class UpdateWorkTypes < ActiveRecord::Migration
+  def self.up
+
+    say_with_time "Updating work subtypes..." do
+      works = Work.find(:all,
+      :conditions => ['type = ? or type = ? or type = ? or type = ?', 'BookEdited', 'JournalEdited', 'SoundRecording', 'Video'])
+      ri_a = Array.new
+      works.each do |w|
+        if w.type.to_s == "BookEdited"
+          w.update_type_and_save('BookWhole')
+          say "Work #{w.id} changed from Book (Edited) to Book (Whole)!", true
+        end
+        if w.type.to_s == "JournalEdited"
+          w.update_type_and_save('JournalWhole')
+          say "Work #{w.id} changed from Journal (Edited) to Journal (Whole)!", true
+        end
+        if w.type.to_s == "SoundRecording"
+          w.update_type_and_save('RecordingSound')
+          say "Work #{w.id} changed from Sound Recording to Recording (Sound)!", true
+        end
+        if w.type.to_s == "Video"
+          w.update_type_and_save('RecordingMovingImage')
+          say "Work #{w.id} changed from Video to Recording (Moving Image)!", true
+        end
+        ri_a << w
+      end
+      Index.batch_update_solr(ri_a)
+    end
+
+  end
+
+  def self.down
+    # No turning back.
+    raise ActiveRecord::IrreversibleMigration, "Sorry, you can't migrate down."
+  end
+end
