cb88db77408300ff0419dbb597965636fedb64d9
ewlarson <ewlarson@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-20 19:06:37 +0000

    Final 1.0 Work Subklasses *-- migration required --*
    Removing all non-1.0, unsupported Work.subklasses.  Converting current ConferenceProceedings to ConferencePapers.  New ConferenceProceedingsWhole subklass is for complete listings of conference papers.
    
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@1217 6a0f952e-c525-0410-88e5-87db5789ed7d

---
+++ db/migrate/20090820185105_final_one_point_oh_work_subklasses.rb
@@ -0,0 +1,28 @@
+class ConferenceProceeding < Work
+  validates_presence_of :title_primary
+end
+
+class FinalOnePointOhWorkSubklasses < ActiveRecord::Migration
+  
+  def self.up
+    
+    say_with_time "Updating work subtypes..." do
+      works = Work.find(:all,
+      :conditions => ['type = ?', 'ConferenceProceeding'])
+      ri_a = Array.new
+      works.each do |w|
+        if w.type.to_s == "ConferenceProceeding"
+          w.update_type_and_save('ConferencePaper')
+          say "Work #{w.id} changed from ConferenceProceeding to ConferencePaper!", true
+        end
+        ri_a << w
+      end
+      Index.batch_update_solr(ri_a)
+    end
+  end
+
+  def self.down
+    # No turning back.
+    raise ActiveRecord::IrreversibleMigration, "Sorry, you can't migrate down."
+  end
+end
