d9a1842f93419b257b52afa556934e645b82360a
waingram <waingram@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-21 20:05:45 +0000

    Bug fix: checking for nil publications

--- app/models/work_subclasses/book_whole.rb
+++ app/models/work_subclasses/book_whole.rb
@@ -20,8 +20,10 @@ class BookWhole < Work
     open_url_kevs[:format]      = "&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook"
     open_url_kevs[:genre]       = "&rft.genre=book"
     open_url_kevs[:title]       = "&rft.btitle=#{CGI.escape(self.title_primary)}"
-    open_url_kevs[:publisher]   = "&rft.pub=#{self.publisher.authority.name}"
-    open_url_kevs[:isbn]        = "&rft.isbn=#{self.publication.isbns.first[:name]}" if !self.publication.isbns.empty?
+    unless self.publisher.nil?
+      open_url_kevs[:publisher]   = "&rft.pub=#{self.publisher.authority.name}"
+      open_url_kevs[:isbn]        = "&rft.isbn=#{self.publication.isbns.first[:name]}" if !self.publication.isbns.empty?
+    end
     open_url_kevs[:date]        = "&rft.date=#{self.publication_date}"
     
     return open_url_kevs
--- app/models/work_subclasses/journal_article.rb
+++ app/models/work_subclasses/journal_article.rb
@@ -20,8 +20,10 @@ class JournalArticle < Work
     open_url_kevs[:format]     = "&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal"
     open_url_kevs[:genre]      = "&rft.genre=article"
     open_url_kevs[:title]      = "&rft.atitle=#{CGI.escape(self.title_primary)}"
-    open_url_kevs[:source]     = "&rft.jtitle=#{self.publication.authority.name}"
-    open_url_kevs[:issn]       = "&rft.issn=#{self.publication.issns.first[:name]}" if !self.publication.issns.empty?
+    unless self.publication.nil?
+      open_url_kevs[:source]     = "&rft.jtitle=#{self.publication.authority.name}"
+      open_url_kevs[:issn]       = "&rft.issn=#{self.publication.issns.first[:name]}" if !self.publication.issns.empty?
+    end
     open_url_kevs[:date]       = "&rft.date=#{self.publication_date}"
     open_url_kevs[:volume]     = "&rft.volume=#{self.volume}"
     open_url_kevs[:issue]      = "&rft.issue=#{self.issue}"
