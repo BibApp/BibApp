67bc68cec768ecb7505faf005c1f2827cea1765f
waingram <waingram@6a0f952e-c525-0410-88e5-87db5789ed7d>
2009-08-21 20:05:45 +0000

    Bug fix: checking for nil publications
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@1235 6a0f952e-c525-0410-88e5-87db5789ed7d

--- app/models/work_subclasses/book_section.rb
+++ app/models/work_subclasses/book_section.rb
@@ -21,8 +21,10 @@ class BookSection < Work
     open_url_kevs[:format]      = "&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook"
     open_url_kevs[:genre]       = "&rft.genre=bookitem"
     open_url_kevs[:title]       = "&rft.title=#{CGI.escape(self.title_primary)}"
-    open_url_kevs[:publisher]   = "&rft.pub=#{self.publisher.authority.name}"
-    open_url_kevs[:isbn]        = "&rft.isbn=#{self.publication.isbns.first[:name]}" if !self.publication.isbns.empty?
+    unless self.publisher.nil?
+      open_url_kevs[:publisher]   = "&rft.pub=#{self.publisher.authority.name}"
+      open_url_kevs[:isbn]        = "&rft.isbn=#{self.publication.isbns.first[:name]}" if !self.publication.isbns.empty?
+    end
     open_url_kevs[:date]        = "&rft.date=#{self.publication_date}"
     
     return open_url_kevs
