54afba5c58ef13162474a72ca07218cf0c099ae1
waingram <waingram@6a0f952e-c525-0410-88e5-87db5789ed7d>
2010-05-20 22:18:01 +0000

    Added RDF mime type for Works.
    ex: /bibapp/works.rdf
    ex: /bibapp/works/2.rdf
    
    git-svn-id: https://bibapp.googlecode.com/svn/trunk@1490 6a0f952e-c525-0410-88e5-87db5789ed7d

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -7,3 +7,6 @@
 #### Register METS as XML MIME Type, so we can provide RESTful METS ####
 Mime::Type.register_alias "text/xml", :mets
 
+#### Register RDF as XML MIME Type, so we can provide RESTful METS ####
+Mime::Type.register_alias "text/xml", :rdf
+
