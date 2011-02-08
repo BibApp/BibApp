d432f750c6859ee8319ebb5405c524d690344a80
Howard Ding <hding2@illinois.edu>
2011-02-08 09:03:45 -0800

    Added more auto generated files in rails 3 upgrade.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibappRails3::Application.config.secret_token = '6d2a9dd5d20c12e6a0e2405102920653083c055d96e0b22d23f8f1c24606c82884e3c551e3008d8a5482c1497b6d042f4b98a9d5936f7b2da14b2beb41859e95'
