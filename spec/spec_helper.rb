974c5a0824e68264102875851694ae3ed5daa93c
Howard Ding <hding2@illinois.edu>
2011-02-14 07:23:51 -0800

    Remove rspec rake tasks (now provided by gem directly). Modify spec_helper - working towards having specs work with Rails 3, although that is still quite in progress.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,67 +1,30 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
 require 'shoulda'
-require 'spec/autorun'
-require 'spec/rails'
-require "authlogic/test_case" # include at the top of test_helper.rb
+require 'authlogic/test_case'
 
-# Uncomment the next line to use webrat's matchers
-#require 'webrat/integrations/rspec-rails'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories. Also require all factories, even in subdirectories
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'factories','**','*.rb'))].each {|f| require f}
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/factories/**/*.rb")].each { |f| require f }
 
 ActionMailer::Base.delivery_method = :test
 
-Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
-  config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = Rails.root.to_s + '/spec/fixtures/'
-
-  # == Fixtures
-  #
-  # You can declare fixtures for each example_group like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so right here. Just uncomment the next line and replace the fixture
-  # names with your fixtures.
-  #
-  # config.global_fixtures = :table_a, :table_b
-  #
-  # config.global_fixtures = :users
-
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  #
+RSpec.configure do |config|
   # == Mock Framework
   #
-  # RSpec uses its own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-  #
-  # == Notes
-  #
-  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+  config.mock_with :rspec
 
   include Authlogic::TestCase
   config.before(:each) do
-    activate_authlogic        
+    activate_authlogic
   end
 
   def login_as(factory = :activated_user, opts = {})
