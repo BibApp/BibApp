8311d23c2719f39e94b25971aaf9f01cf1cf12bc
Howard Ding <hding2@illinois.edu>
2011-02-17 09:59:18 -0800

    Fix spec_test vs. contributorships_controller problem that cropped up after making the make_resourceful change.

--- app/controllers/contributorships_controller.rb
+++ app/controllers/contributorships_controller.rb
@@ -1,33 +1,33 @@
 class ContributorshipsController < ApplicationController
-  
+
   #Require a user be logged in to create / update / destroy
-  before_filter :login_required, :only => [ :new, :create, :edit, :update, :destroy ]
-  
+  before_filter :login_required, :only => [:new, :create, :edit, :update, :destroy]
+
   make_resourceful do
     build :index
-    
+
     before :index do
       if params[:person_id]
         @person = Person.find(params[:person_id])
-        @page   = params[:page] || 1
+        @page = params[:page] || 1
         @rows = params[:rows] || 10
         @status = params[:status] || "unverified"
-        
+
         @contributorships = @person.contributorships.send(@status).paginate(
-          :page => @page,
-          :per_page => @rows,
-          :include => [:work],
-          :order => 'works.publication_date desc'
+            :page => @page,
+                :per_page => @rows,
+                :include => [:work],
+                :order => 'works.publication_date desc'
         )
       else
-        render :controller => "application", :action => "error_404"
+        render :status => 404
       end
     end
   end
-  
+
   def admin
 
-    @people = Contributorship.unverified.visible.group_by{|c| c.person_id}
+    @people = Contributorship.unverified.visible.group_by { |c| c.person_id }
     t = true
   end
 
@@ -53,10 +53,10 @@ class ContributorshipsController < ApplicationController
         contributorship.verify_contributorship
       end
     end
-    
+
     #Return path for any actions that take place on the contributorships page
     return_path = contributorships_path(:person_id=>params[:person_id],
-                                        :status=>params[:status])
+        :status=>params[:status])
 
     respond_to do |format|
       if full_success
@@ -65,8 +65,8 @@ class ContributorshipsController < ApplicationController
         flash[:warning] = "One or more contributorships could not be verified; you have insufficient privileges"
       end
       #forward back to path which was specified in params
-      format.html {redirect_to return_path }
-      format.xml  {head :ok}
+      format.html { redirect_to return_path }
+      format.xml { head :ok }
     end
   end
 
@@ -98,7 +98,7 @@ class ContributorshipsController < ApplicationController
 
     #Return path for any actions that take place on the contributorships page
     return_path = contributorships_path(:person_id=>params[:person_id],
-                                        :status=>params[:status])
+        :status=>params[:status])
 
     respond_to do |format|
       if full_success
@@ -107,8 +107,8 @@ class ContributorshipsController < ApplicationController
         flash[:warning] = "One or more contributorships could not be unverified; you have insufficient privileges"
       end
       #forward back to path which was specified in params
-      format.html {redirect_to return_path }
-      format.xml  {head :ok}
+      format.html { redirect_to return_path }
+      format.xml { head :ok }
     end
   end
 
@@ -140,7 +140,7 @@ class ContributorshipsController < ApplicationController
 
     #Return path for any actions that take place on the contributorships page
     return_path = contributorships_path(:person_id=>params[:person_id],
-                                        :status=>params[:status])
+        :status=>params[:status])
 
     respond_to do |format|
       if full_success
@@ -149,94 +149,94 @@ class ContributorshipsController < ApplicationController
         flash[:warning] = "One or more contributorships could not be denied; you have insufficient privileges"
       end
       #forward back to path which was specified in params
-      format.html {redirect_to return_path }
-      format.xml  {head :ok}
+      format.html { redirect_to return_path }
+      format.xml { head :ok }
     end
   end
-  
+
   def verify
     @contributorship = Contributorship.find(params[:id])
     person = @contributorship.person
-    
+
     # only 'editor' of this person can verify contributorship   
     permit "editor of :person", :person => person
-    
+
     #Verify & save contributorship
     #(Note: Contributorship callbacks will automatically update scores, etc.)
     @contributorship.verify_contributorship
     @contributorship.save
-    
+
     #get updated list of contributorships to display
     @contributorships = person.contributorships.to_show
-    
+
     respond_to do |format|
       format.html { redirect_to :back }
-      format.js   { render :action => :verify_contributorship }
+      format.js { render :action => :verify_contributorship }
     end
   end
-  
+
   def deny
     # Find Contributorship
     @contributorship = Contributorship.find(params[:id])
     @person = Person.find_by_id(@contributorship.person_id)
-    
+
     # only 'editor' of this person can deny contributorship   
     permit "editor of person"
-    
+
     @contributorship.deny_contributorship
     @contributorship.save
-   
+
     # RJS action removes the denied Work from the view
     respond_to do |format|
       format.html { redirect_to :back }
-      format.js   { render :action => :deny_contributorship }
+      format.js { render :action => :deny_contributorship }
     end
   end
-  
+
   def archivable
     # Find Person for view
     @person = Person.find(params[:person_id])
-    
+
     # Collect data for Sherpa color table
     @pub_table = romeo_color_count
-    
+
     # Calculate the sum of each Sherpa color
-    @pub_totals = @pub_table.collect{|c| c.count}.inject{|sum, n| sum.to_i + n.to_i}
-    
+    @pub_totals = @pub_table.collect { |c| c.count }.inject { |sum, n| sum.to_i + n.to_i }
+
     # Collect data for Publication table
     @publ_table = publication_count
   end
-  
+
   private
-  
+
   def romeo_color_count
     # Build query which groups all Works (of this person) 
     # under appropriate Romeo Colors (based on publisher)
     # and retrieves a total number of each Romeo Color.
     Contributorship.verified.all(
-                         :select => "count(contributorships.id) as count, publishers.romeo_color as color", 
-                         :joins => "JOIN works ON contributorships.work_id=works.id
+        :select => "count(contributorships.id) as count, publishers.romeo_color as color",
+            :joins => "JOIN works ON contributorships.work_id=works.id
                                     JOIN people ON contributorships.person_id=people.id
                                     JOIN publishers ON works.publisher_id=publishers.id",
-                         :conditions => ["people.id = ?", @person.id],
-                         :group => "publishers.romeo_color",
-                         :order => "publishers.romeo_color")
+            :conditions => ["people.id = ?", @person.id],
+            :group => "publishers.romeo_color",
+            :order => "publishers.romeo_color")
   end
-  
-  
+
+
   def publication_count
     # Build query which groups all works (of this person) 
     # by the Journal/Publication and Publisher
     # and retrieves a total number of each Journal/Publication
     Contributorship.verified.all(
-                         :select => "count(contributorships.id) as count, publications.name as name, publishers.name as pub_name, publishers.romeo_color as color, publishers.publisher_copy as pub_copy",
-                         :joins => "JOIN works ON contributorships.work_id=works.id
+        :select => "count(contributorships.id) as count, publications.name as name, publishers.name as pub_name, publishers.romeo_color as color, publishers.publisher_copy as pub_copy",
+            :joins => "JOIN works ON contributorships.work_id=works.id
                                     JOIN people ON contributorships.person_id=people.id
                                     JOIN publications ON works.publication_id = publications.id
                                     JOIN publishers ON works.publisher_id=publishers.id",
-                         :conditions => ["people.id = ?", @person.id],
-                         :group => "publications.name, publishers.name, publishers.romeo_color, publishers.publisher_copy",
-                         :order => "count(contributorships.id) desc")
+            :conditions => ["people.id = ?", @person.id],
+            :group => "publications.name, publishers.name, publishers.romeo_color, publishers.publisher_copy",
+            :order => "count(contributorships.id) desc")
   end
-  
+
 end
--- spec/controllers/contributorship_controller_spec.rb
+++ spec/controllers/contributorship_controller_spec.rb
@@ -6,10 +6,18 @@ describe ContributorshipsController do
     @contributorship = Factory.create(:contributorship)
   end
 
-  it "should get index" do
-    get :index
-    response.should be_success
-    assigns(:contributorships).should_not be_nil
+  context "index" do
+    it "should get given a person_id" do
+      person = Factory.create(:person)
+      get :index, :person_id => person.id
+      response.should be_success
+      assigns(:contributorships).should_not be_nil
+    end
+
+    it "should redirect to 404 not given a person_id" do
+      get :index
+      response.status.should == 404
+    end
   end
 
   context "logged in" do
