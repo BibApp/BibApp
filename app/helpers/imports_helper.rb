298a9fdff38dca870dce7500e0bbc5ae2da0cd55
Howard Ding <hding2@illinois.edu>
2011-02-24 12:07:23 -0800

    refactoring changed function name

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -13,7 +13,7 @@ module ImportsHelper
     classes = name[1][:works].collect{|w| "work-#{w}"}.join(" ")
     capture_haml do
       haml_tag :li, {:id => "ns-#{name[1][:id]}", :class => "#{classes}", :style => "border-bottom:1px solid #CCC; list-style:none; line-height:1.75em;"} do
-        haml_tag :span, ajax_checkbox_toggle(NameString.find_by_id(name[1][:id]), person, false, true)
+        haml_tag :span, ajax_pen_name_checkbox_toggle(NameString.find_by_id(name[1][:id]), person, false, true)
         haml_tag :span, "#{name[0]} (#{name[1][:works].size})", {:class => "name_string span-5 prepend-1" }
         haml_tag :span, "#{name[1][:works].size}", {:id => "ns-#{name[1][:id]}-count", :class=> "hide"}
       end
