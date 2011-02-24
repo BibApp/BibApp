c8d9dc1e6eacf97fdc476e31f9fc1da42aeac7bf
Howard Ding <hding2@illinois.edu>
2011-02-24 14:45:14 -0800

    fixed deprecation

--- vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu.rb
+++ vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu.rb
@@ -1,7 +1,7 @@
 module Technoweenie # :nodoc:
   module AttachmentFu # :nodoc:
     @@default_processors = %w(ImageScience Rmagick MiniMagick Gd2 CoreImage)
-    @@tempfile_path      = File.join(RAILS_ROOT, 'tmp', 'attachment_fu')
+    @@tempfile_path      = File.join(Rails.root, 'tmp', 'attachment_fu')
     @@content_types      = [
       'image/jpeg',
       'image/pjpeg',
