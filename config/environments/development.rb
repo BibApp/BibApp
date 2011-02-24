c56e870938c4f08fb5453a3f08b28c46f0ffd273
Howard Ding <hding2@illinois.edu>
2011-02-24 15:05:03 -0800

    Fix deprecation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Bibapp::Application.configure do
   config.whiny_nils = true
 
 # Show full error reports and disable caching
-  config.action_controller.consider_all_requests_local = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = false
   config.action_view.debug_rjs = true
 
