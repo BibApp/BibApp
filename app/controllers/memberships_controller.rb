7eb83a09864ced2c2f0e016136d6d99083fc732a
Howard Ding <hding2@illinois.edu>
2011-02-25 15:53:43 -0800

    Updated query syntax

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,15 +1,15 @@
 class MembershipsController < ApplicationController
 
   #Require a user be logged in to create / update / destroy
-  before_filter :login_required, :only => [ :new, :create, :edit, :update, :destroy]
-  
+  before_filter :login_required, :only => [:new, :create, :edit, :update, :destroy]
+
   before_filter :find_membership, :only => [:destroy]
   before_filter :find_person, :only => [:create, :create_group, :new, :destroy, :sort]
-  before_filter :find_group,  :only => [:create, :create_group, :destroy]
-  
-  make_resourceful do 
+  before_filter :find_group, :only => [:create, :create_group, :destroy]
+
+  make_resourceful do
     build :index, :show, :new, :update
-    
+
     before :update do
       #'editor' of person or group can update membership details
       permit "editor of person or group"
@@ -34,24 +34,19 @@ class MembershipsController < ApplicationController
 
         #Chop five times to remove the trailing " AND "
         like_string = like_string.chop.chop.chop.chop.chop
-        
+
         #Add the "LIKE" string to the array
         query_string = []
         query_string << like_string
-        
+
         #Add the search terms
         query_words.each do |qw|
           query_string << ("%" + qw + "%")
         end
 
-        results = Group.find(
-          :all,
-          :conditions => query_string,
-          :order => "name ASC")
+        results = Group.where(query_string).order_by_name
       else
-        results = Group.find(
-          :all,
-          :order => "name ASC")
+        results = Group.order_by_name
       end
 
       @parents = Array.new
@@ -77,23 +72,23 @@ class MembershipsController < ApplicationController
         @groups << g.ancestors_and_descendants
         @groups << g
       end
-      
-      @groups  = @groups.flatten.uniq
+
+      @groups = @groups.flatten.uniq
       @parents.uniq!
 
-      @groups.sort! { |a,b| a.name.downcase <=> b.name.downcase }
-      @parents.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+      @groups.sort! { |a, b| a.name.downcase <=> b.name.downcase }
+      @parents.sort! { |a, b| a.name.downcase <=> b.name.downcase }
 
     end
 
   end
-  
-  
+
+
   def create
     #'editor' of person or group can create a membership
     permit "editor of person or group"
-    
-    @person.groups << @group  
+
+    @person.groups << @group
     respond_to do |format|
       format.js { render :action => :regen_lists }
       format.html { redirect_to new_person_membership_path(:person_id => @person.id) }
@@ -128,7 +123,7 @@ class MembershipsController < ApplicationController
 
     #Return path for any actions that take place on the memberships page
     return_path = new_person_membership_path(:person_id=>params[:person_id],
-                                   :status=>full_success ? 'member' : params[:status])
+                                             :status=>full_success ? 'member' : params[:status])
 
     respond_to do |format|
       if full_success
@@ -137,37 +132,34 @@ class MembershipsController < ApplicationController
         flash[:warning] = "One or more groups could not be joined; you have insufficient privileges"
       end
       #forward back to path which was specified in params
-      format.html {redirect_to return_path }
-      format.xml  {head :ok}
+      format.html { redirect_to return_path }
+      format.xml { head :ok }
     end
   end
-  
+
   def create_group
     #'editor' of person can create new groups
     permit "editor of person"
-    
+
     @group = Group.find_or_create_by_name(params[:group][:name])
     @group.hide = false
     @group.save
-    
-    @membership = Membership.find_by_person_id_and_group_id(
-     		@person.id,
-     		@group.id)
-    if(@membership == nil)
-    	@person.groups << @group
-    	respond_to do |format|
-      	format.html { redirect_to new_person_membership_path(:person_id => @person.id) }
-      	format.js { render :action => :regen_lists }
-    	end
-  	end
-  end
 
+    @membership = Membership.find_by_person_id_and_group_id(@person.id, @group.id)
+    unless @membership
+      @person.groups << @group
+      respond_to do |format|
+        format.html { redirect_to new_person_membership_path(:person_id => @person.id) }
+        format.js { render :action => :regen_lists }
+      end
+    end
+  end
 
   def search_groups
     respond_to do |format|
       format.js { render :action => :regen_lists }
       format.html { redirect_to new_person_membership_path(:person_id => params[:person_id],
-          :status => params[:status], :q => params[:q]) }
+                                                           :status => params[:status], :q => params[:q]) }
     end
   end
 
@@ -175,7 +167,7 @@ class MembershipsController < ApplicationController
   def destroy
     #'editor' of person or group can destroy a membership
     permit "editor of person or group"
-    
+
     @membership.destroy if @membership
     respond_to do |format|
       format.js { render :action => :regen_lists }
@@ -183,14 +175,14 @@ class MembershipsController < ApplicationController
     end
 
   end
-  
+
   def sort
     @person.groups.each do |group|
       membership = Membership.find_by_person_id_and_group_id(@person.id, group.id)
       membership.position = params["current"].index(group.id.to_s)+1
       membership.save
     end
-    
+
     respond_to do |format|
       format.js { render :action => :regen_lists }
       format.html { redirect_to new_membership_path(:person_id => @person.id) }
@@ -202,40 +194,35 @@ class MembershipsController < ApplicationController
     membership = Membership.find(params[:id])
     membership.update_attributes(params[:membership])
     @person = membership.person
-    
+
     render :partial => 'group', :collection => @person.groups(true)
-    
+
   end
-  
+
   def auto_complete_for_group_name
     group_name = params[:group][:name].downcase
-    
+
     #search at beginning of name
     beginning_search = group_name + "%"
     #search at beginning of any other words in name
     word_search = "% " + group_name + "%"
-    
-    groups = Group.find(:all, 
-          :conditions => [ "(LOWER(name) LIKE ? OR LOWER(name) LIKE ?) AND hide = ?", beginning_search, word_search, false ], 
-        :order => 'name ASC',
-        :limit => 8)
-      
+
+    groups = Group.unhidden.where("LOWER(name) LIKE ? OR LOWER(name) LIKE ?",
+                                  beginning_search, word_search).order_by_name.limit(8)
+
     render :partial => 'autocomplete_list', :locals => {:objects => groups}
-  end 
-  
+  end
+
   private
   def find_person
     @person = Person.find_by_id(params[:person_id])
   end
-  
+
   def find_group
     @group = Group.find_by_id(params[:group_id])
   end
-  
+
   def find_membership
-    @membership = Membership.find_by_person_id_and_group_id(
-      params[:person_id],
-      params[:group_id]
-    )
+    @membership = Membership.find_by_person_id_and_group_id(params[:person_id],params[:group_id])
   end
 end
