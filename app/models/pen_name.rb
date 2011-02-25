2d3d731dc8eabb20010021170319fd8e360c41cd
Howard Ding <hding2@illinois.edu>
2011-02-25 13:26:10 -0800

    Was editing this to remove some to be deprecated find calls. In the process did some cleanup: Added works relationship through contributorships and used it where this was done manually. Destroy contributorships automatically on destruction of PenName. Remove redundant find_contributorships method. Clean up creation of new contributorships by doing it through the relation.

--- app/models/pen_name.rb
+++ app/models/pen_name.rb
@@ -3,7 +3,8 @@
 class PenName < ActiveRecord::Base
   belongs_to :name_string
   belongs_to :person
-  has_many :contributorships
+  has_many :contributorships, :dependent => :destroy
+  has_many :works, :through => :contributorships
 
   validates_presence_of :name_string_id, :person_id
 
@@ -11,45 +12,23 @@ class PenName < ActiveRecord::Base
   before_destroy :before_destroy_actions
 
   def after_save_actions
-    # create new contributorships
-    contributorships = self.set_contributorships
-
-    works = contributorships.collect { |c| c.work }
-
-    works.each { |work| work.save_and_set_for_index }
+    self.set_contributorships
+    self.works.each { |work| work.save_and_set_for_index }
     Index.batch_index
   end
 
   def before_destroy_actions
-    contributorships = self.find_contributorships
-
-    works = contributorships.collect { |c| c.work }
-
-    works.each { |work| work.save_and_set_for_index }
+    self.works.each { |w| w.save_and_set_for_index }
     Index.batch_index
-
-    contributorships.each { |c| c.destroy }
   end
 
   def set_contributorships
-    contributorships = Array.new
     self.name_string.work_name_strings.each do |cns|
       #only create Contributorship for "accepted" works
       if cns.work.accepted?
-        contributorships << Contributorship.find_or_create_by_work_id_and_person_id_and_pen_name_id_and_role(
-            cns.work.id,
-                self.person_id,
-                self.id,
-                cns.role
-        )
+        self.contributorships.find_or_create_by_work_id_and_person_id_and_role(cns.work.id, self.person_id, cns.role)
       end
     end
-    return contributorships
   end
 
-  # Find all Contributorships associated with this PenName
-  def find_contributorships
-    Contributorship.find(:all,
-            :conditions => ["pen_name_id = ? and person_id = ?", self.id, self.person_id])
-  end
 end
