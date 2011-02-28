eaf5bca12fa89790bee7cc711fdbe6fa3a98eb16
Howard Ding <hding2@illinois.edu>
2011-02-28 14:39:02 -0800

    Changed method calls for model refactoring.

--- db/migrate/20090814194316_update_work_types.rb
+++ db/migrate/20090814194316_update_work_types.rb
@@ -19,19 +19,19 @@ class UpdateWorkTypes < ActiveRecord::Migration
       ri_a = Array.new
       works.each do |w|
         if w.type.to_s == "BookEdited"
-          w.update_type_and_save_without_callbacks('BookWhole')
+          w.update_type_and_save('BookWhole')
           say "Work #{w.id} changed from Book (Edited) to Book (Whole)!", true
         end
         if w.type.to_s == "JournalEdited"
-          w.update_type_and_save_without_callbacks('JournalWhole')
+          w.update_type_and_save('JournalWhole')
           say "Work #{w.id} changed from Journal (Edited) to Journal (Whole)!", true
         end
         if w.type.to_s == "SoundRecording"
-          w.update_type_and_save_without_callbacks('RecordingSound')
+          w.update_type_and_save('RecordingSound')
           say "Work #{w.id} changed from Sound Recording to Recording (Sound)!", true
         end
         if w.type.to_s == "Video"
-          w.update_type_and_save_without_callbacks('RecordingMovingImage')
+          w.update_type_and_save('RecordingMovingImage')
           say "Work #{w.id} changed from Video to Recording (Moving Image)!", true
         end
         ri_a << w
