6159d1b5e79ec1b8d91fe2fae123337eebf8767c
Howard Ding <hding2@illinois.edu>
2011-02-28 13:23:50 -0800

    change delete_all to destroy

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  has_many :taggings, :dependent => :delete_all
+  has_many :taggings, :dependent => :destroy
   has_many :tags, :through => :taggings
   has_many :users, :through => :taggings
 
