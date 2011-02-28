b213bbbbeabbe08f13db01da2eac792bc709038f
Howard Ding <hding2@illinois.edu>
2011-02-28 09:22:10 -0800

    refactor a bit

--- app/models/pen_name.rb
+++ app/models/pen_name.rb
@@ -8,21 +8,11 @@ class PenName < ActiveRecord::Base
 
   validates_presence_of :name_string_id, :person_id
 
-  after_save :after_save_actions
-  before_destroy :before_destroy_actions
+  after_save :set_contributorships
+  after_save :index_works
+  before_destroy :index_works
 
   scope :for_name_string, lambda {|name_string_id| where(:name_string_id => name_string_id)}
-
-  def after_save_actions
-    self.set_contributorships
-    self.works.each { |work| work.save_and_set_for_index }
-    Index.batch_index
-  end
-
-  def before_destroy_actions
-    self.works.each { |w| w.save_and_set_for_index }
-    Index.batch_index
-  end
 
   def set_contributorships
     self.name_string.work_name_strings.each do |cns|
@@ -31,6 +21,11 @@ class PenName < ActiveRecord::Base
         self.contributorships.find_or_create_by_work_id_and_person_id_and_role(cns.work.id, self.person_id, cns.role)
       end
     end
+  end
+
+  def index_works
+    self.works.each { |w| w.save_and_set_for_index }
+    Index.batch_index
   end
 
 end
