1b515a071a173d8c781ddcc94b29cb092c430fd1
Howard Ding <hding2@illinois.edu>
2011-02-28 14:42:34 -0800

    minor cleanup

--- app/models/work_name_string.rb
+++ app/models/work_name_string.rb
@@ -1,31 +1,30 @@
-class WorkNameString   < ActiveRecord::Base
+class WorkNameString < ActiveRecord::Base
   belongs_to :name_string
   belongs_to :work
   acts_as_list :scope => :work_id
-  
+
   validates_presence_of :name_string_id, :work_id
-  #validates_uniqueness_of :work_id, :scope => :name_string_id
   validates_uniqueness_of :name_string_id, :scope => [:work_id, :role], :case_sensitive => true
 
-  scope :with_role, lambda {|role| where(:role => role)}
+  scope :with_role, lambda { |role| where(:role => role) }
 
   # Convert object into semi-structured data to be stored in Solr
   def to_solr_data
     "#{self.name_string.name}||#{self.name_string.id}||#{position}||#{role}"
   end
-  
-  class << self
-    def create_batch!(name_string_id, work_data)
-      cites = Work.import_batch!(work_data)
-      create_batch_from_works!(name_string_id, cites)
-      return cites
-    end
-  
-    def create_batch_from_works!(name_string, works)
-      return if not works.respond_to? :each
-      works.each do |c|
-        WorkNameString.create(:name_string_id => name_string.id, :work_id => c.id )
-      end
+
+
+  def self.create_batch!(name_string_id, work_data)
+    cites = Work.import_batch!(work_data)
+    create_batch_from_works!(name_string_id, cites)
+    return cites
+  end
+
+  def self.create_batch_from_works!(name_string, works)
+    return unless works.respond_to?(:each)
+    works.each do |c|
+      WorkNameString.create(:name_string_id => name_string.id, :work_id => c.id)
     end
   end
+
 end
