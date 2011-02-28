8f6c3636d98d1cc95b1a813860fcce10edee97f7
Howard Ding <hding2@illinois.edu>
2011-02-28 08:28:51 -0800

    Remove no longer supported save_without_callbacks. Make update_machine_name a before_save callback (which should eliminate the need to explicitly save it anyway).

--- app/models/external_system.rb
+++ app/models/external_system.rb
@@ -6,14 +6,8 @@ class ExternalSystem < ActiveRecord::Base
   validates_presence_of :name, :base_url
    
   #### Callbacks ####
-  after_save :update_machine_name
-  
-  #### Methods ####
-  
-  def save_without_callbacks
-    update_without_callbacks
-  end
-  
+  before_save :update_machine_name
+
   #Update Machine Name of ExternalSystem (called by after_save callback)
   def update_machine_name
     #Machine name only needs updating if there was a name change
@@ -22,7 +16,6 @@ class ExternalSystem < ActiveRecord::Base
       #  1. all punctuation/spaces converted to single space
       #  2. stripped of leading/trailing spaces and downcased
       self.machine_name = self.name.mb_chars.gsub(/[\W]+/, " ").strip.downcase
-      self.save_without_callbacks
     end
   end
 end
