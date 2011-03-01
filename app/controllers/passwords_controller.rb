0f65faaf1151150bf1059c0fcaa88234fcbd299c
Howard Ding <hding2@illinois.edu>
2011-03-01 08:59:18 -0800

    formatting, small amount of cleanup

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,66 +2,66 @@
 #   Code borrowed from: http://blog.compulsivoco.com/2008/03/24/how-to-change-or-reset-your-password-with-restful_authentication/
 class PasswordsController < ApplicationController
 
-   # require user is logged in, except for "forgot password" page
-   before_filter :login_required, :except => [:create, :new]  
-  
-   # POST /passwords  
-   # Forgot password  
-   def create  
-     respond_to do |format|  
-   
-       if user = User.find_by_email_and_login(params[:email], params[:login])  
-         @new_password = random_password  
-         user.password = user.password_confirmation = @new_password  
-         user.save_without_validation  
-         UserMailer.new_password(user, @new_password).deliver
-   
-         format.html {  
-           flash[:notice] = "We sent a new password to #{params[:email]}"  
-           redirect_to login_path  
-         }  
-       else  
-         flash[:notice] =  "Sorry, we cannot find that account.  Try again."  
-         format.html { render :action => "new" }  
-       end  
-     end  
-   end    
-   
-   # GET /users/1/password/edit  
-   # Changing password  
-   def edit  
-     @user = current_user  
-   end  
-   
-   # PUT /users/1/password  
-   # Changing password  
-   def update  
-     @user = current_user  
-   
-     old_password = params[:old_password]  
-   
-     @user.attributes = params[:user]  
-   
-     respond_to do |format|  
-       if @user.valid_password?(old_password) && @user.save
-         flash[:notice] = "Your password was updated successfully."
-         format.html { redirect_to edit_user_path(@user) }  
-       else
-         if !@user.errors.empty?
-           flash[:notice] = "Sorry, your new password didn't match the confirmation.  Try again."
-         else
-           flash[:notice] = "Sorry, your old password was incorrect. Try again." 
-         end
-         format.html { render :action => 'edit' }  
-       end  
-     end  
-   end  
-   
-   protected   
-   
-   def random_password( len = 20 )  
-     chars = (("a".."z").to_a + ("1".."9").to_a )- %w(i o 0 1 l 0)  
-     newpass = Array.new(len, '').collect{chars[rand(chars.size)]}.join  
-   end  
-   
- end  
+  # require user is logged in, except for "forgot password" page
+  before_filter :login_required, :except => [:create, :new]
+
+  # POST /passwords
+  # Forgot password
+  def create
+    respond_to do |format|
+
+      if user = User.find_by_email_and_login(params[:email], params[:login])
+        @new_password = random_password
+        user.password = user.password_confirmation = @new_password
+        user.save_without_validation
+        UserMailer.new_password(user, @new_password).deliver
+
+        format.html do
+          flash[:notice] = "We sent a new password to #{params[:email]}"
+          redirect_to login_path
+        end
+      else
+        flash[:notice] = "Sorry, we cannot find that account.  Try again."
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
+  # GET /users/1/password/edit
+  # Changing password
+  def edit
+    @user = current_user
+  end
+
+  # PUT /users/1/password
+  # Changing password
+  def update
+    @user = current_user
+
+    old_password = params[:old_password]
+
+    @user.attributes = params[:user]
+
+    respond_to do |format|
+      if @user.valid_password?(old_password) && @user.save
+        flash[:notice] = "Your password was updated successfully."
+        format.html { redirect_to edit_user_path(@user) }
+      else
+        if !@user.errors.empty?
+          flash[:notice] = "Sorry, your new password didn't match the confirmation.  Try again."
+        else
+          flash[:notice] = "Sorry, your old password was incorrect. Try again."
+        end
+        format.html { render :action => 'edit' }
+      end
+    end
+  end
+
+  protected
+
+  def random_password(len = 20)
+    chars = (("a".."z").to_a + ("1".."9").to_a)- %w(i o 0 1 l 0)
+    Array.new(len, '').collect { chars[rand(chars.size)] }.join
+  end
+
+end
