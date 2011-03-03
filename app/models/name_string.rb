cb8cec16c075cc3815290f7cd15205837c328e86
Howard Ding <hding2@illinois.edu>
2011-03-03 15:03:49 -0800

    small simplification

--- app/models/name_string.rb
+++ app/models/name_string.rb
@@ -35,7 +35,7 @@ class NameString < ActiveRecord::Base
 
   def to_param
     param_name = name.gsub("-", "_")
-    param_name = param_name.gsub(/[^A-Za-z0-9_]/, "")
+    param_name.gsub!(/[^A-Za-z0-9_]/, "")
     "#{id}-#{param_name}"
   end
 
