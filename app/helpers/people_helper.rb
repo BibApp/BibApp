802871a8a8d0d07b322b5a5be05286fd8fa1d5af
Howard Ding <hding2@illinois.edu>
2011-04-15 13:32:48 -0700

    Fixed problem when ldap record had certain fields nil

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -11,8 +11,8 @@ module PeopleHelper
   
   def pretty_ldap_dept(ldap_result)
     ar = Array.new
-    ar << ldap_result[:title].titleize unless ldap_result[:title].empty?
-    ar << ldap_result[:ou].titleize unless ldap_result[:ou].empty?
+    ar << ldap_result[:title].titleize unless ldap_result[:title].blank?
+    ar << ldap_result[:ou].titleize unless ldap_result[:ou].blank?
     ar.size > 0 ? "(#{ar.join(', ')})" : ""
   end
   
