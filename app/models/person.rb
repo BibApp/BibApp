4c1c5a33e45b0c29ac03890a638712071935f2b2
Howard Ding <hding2@illinois.edu>
2011-04-15 07:43:24 -0700

    Added more specs for person and some cleanup of the model

--- app/models/person.rb
+++ app/models/person.rb
@@ -224,8 +224,7 @@ class Person < ActiveRecord::Base
   end
 
   def publication_reftypes
-    Work.select('type, count(type) AS count').
-        joins(:contributorships).
+    Work.select('type, count(type)').joins(:contributorships).
         where(:contributorships => {:person_id => self.id, :contributorship_state_id => Contributorship::STATE_VERIFIED}).
         group('type').order('count desc')
   end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,18 +8,18 @@ describe Person do
 
   describe "associations" do
     it { should belong_to(:user) }
-    it { should have_many(:pen_names).dependent(:destroy)}
-    it {should have_many(:name_strings).through(:pen_names)}
-    it {should have_many(:memberships).dependent(:destroy)}
-    it {should have_many(:groups).through(:memberships)}
-    it {should have_many(:contributorships).dependent(:destroy)}
-    it {should have_many(:works).through(:contributorships)}
-    it {should have_one(:image).dependent(:destroy)}
-    it {should belong_to(:user)}
+    it { should have_many(:pen_names).dependent(:destroy) }
+    it { should have_many(:name_strings).through(:pen_names) }
+    it { should have_many(:memberships).dependent(:destroy) }
+    it { should have_many(:groups).through(:memberships) }
+    it { should have_many(:contributorships).dependent(:destroy) }
+    it { should have_many(:works).through(:contributorships) }
+    it { should have_one(:image).dependent(:destroy) }
+    it { should belong_to(:user) }
   end
 
   describe "validations" do
-    it {should validate_presence_of(:uid)}
+    it { should validate_presence_of(:uid) }
   end
 
   describe "name construction methods and related" do
@@ -72,7 +72,7 @@ describe Person do
       end
       Person.letters.should == ['A', 'M', 'R']
     end
-    
+
   end
 
   describe "finding related objects" do
@@ -85,8 +85,7 @@ describe Person do
       @person.most_recent_work.should be_nil
       works = 5.times.collect do
         Factory.create(:work).tap do |work|
-          contributorship = Factory.create(:contributorship, :work => work, :person => @person)
-          contributorship.verify_contributorship
+          add_contributorship(@person, work)
         end
       end
       work = works.third
@@ -95,25 +94,24 @@ describe Person do
     end
 
     it "can find all groups in which the person is not" do
-      @person.groups = 3.times.collect {Factory.create(:group)}
-      other_groups = 10.times.collect {Factory.create(:group)}
+      @person.groups = 3.times.collect { Factory.create(:group) }
+      other_groups = 10.times.collect { Factory.create(:group) }
       @person.groups_not.to_set.should == other_groups.to_set
     end
 
     it "can find name strings like the person's last name but which the person does not have" do
       @person.name_strings.should_not be_empty
-      other_name_strings = ('aa'..'af').collect {|letter| Factory.create(:name_string, :name => "#{@person.last_name}, #{letter}")}
+      other_name_strings = ('aa'..'af').collect { |letter| Factory.create(:name_string, :name => "#{@person.last_name}, #{letter}") }
       @person.name_strings_not.to_set.should == other_name_strings.to_set
     end
 
   end
 
   it "can sort an array of persons by the time of their most recent work" do
-    none, old, new = 3.times.collect {Factory.create(:person)}
-    old.contributorships << Factory.create(:contributorship, :work => Factory.create(:work), :person => old)
+    none, old, new = 3.times.collect { Factory.create(:person) }
+    add_contributorship(old, nil)
     sleep 1
-    new.contributorships << Factory.create(:contributorship, :work => Factory.create(:work), :person => new)
-    (old.contributorships + new.contributorships).each {|c| c.verify_contributorship}
+    add_contributorship(new, nil)
     unordered = [old, none, new]
     Person.sort_by_most_recent_work(unordered).should == [new, old, none]
   end
@@ -129,7 +127,7 @@ describe Person do
     end
 
     it "can return conversion of person to solr data" do
-      @person.should_receive(:group_ids).and_return([2,4,3])
+      @person.should_receive(:group_ids).and_return([2, 4, 3])
       @person.to_solr_data.should == "Peters||#{@id}||man.jpg||2,4,3||true||#{@person.person_research_focus}"
     end
 
@@ -157,8 +155,31 @@ describe Person do
 
   it "can return a comma separated list of its group ids" do
     person = Person.new
-    person.should_receive(:group_ids).and_return([2,4,3])
+    person.should_receive(:group_ids).and_return([2, 4, 3])
     person.comma_separated_group_ids.should == '2,4,3'
+  end
+
+  it "can return an array of work types and counts of that work type for a person" do
+    person = Factory.create(:person)
+    {:book_whole => [2, 2], :book_review => [1, 0], :composition => [0,2],
+     :grant => [3, 1]}.each do |k,v|
+      v.first.times {add_contributorship(person, Factory.create(k), true)}
+      v.second.times {add_contributorship(person, Factory.create(k), false)}
+    end
+    reftypes = person.publication_reftypes.all
+    reftypes.size.should == 3
+    reftypes.first.type.should == 'Grant'
+    reftypes.first.count.should == '3'
+    reftypes.third.type.should == 'BookReview'
+    reftypes.third.count.should == '1'
+  end
+
+  def add_contributorship(person, work, verified = true)
+    person ||= Factory.create(:person)
+    work ||= Factory.create(:work)
+    person.contributorships << (c = Factory.create(:contributorship, :person => person, :work => work))
+    c.verify_contributorship if verified
+    c
   end
 
 end
