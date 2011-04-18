c400f12c84dada8cd3d83bc27ae30d760156ef19
Howard Ding <hding2@illinois.edu>
2011-04-18 11:47:03 -0700

    n^2 select issue

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,25 +1,25 @@
 class PublicationsController < ApplicationController
 
   #Require a user be logged in to create / update / destroy
-  before_filter :login_required, :only => [ :new, :create, :edit, :update, :destroy ]
-  
+  before_filter :login_required, :only => [:new, :create, :edit, :update, :destroy]
+
   make_resourceful do
     build :index, :show, :new, :edit, :create, :update
-    
+
     publish :yaml, :xml, :json, :attributes => [
-      :id, :name, :url, :issn_isbn, :publisher_id, {
-        :publisher => [:id, :name]
+        :id, :name, :url, :issn_isbn, :publisher_id, {
+            :publisher => [:id, :name]
         }, {
-        :authority => [:id, :name]
+            :authority => [:id, :name]
         }, {
-        :works => [:id]
+            :works => [:id]
         }
-      ]
-      
+    ]
+
     #Add a response for RSS
-    response_for :show do |format| 
-      format.html  #loads show.html.haml (HTML needs to be first, so I.E. views it by default)
-      format.rss  #loads show.rss.rxml
+    response_for :show do |format|
+      format.html #loads show.html.haml (HTML needs to be first, so I.E. views it by default)
+      format.rss #loads show.rss.rxml
     end
 
     response_for :update do |format|
@@ -42,9 +42,10 @@ class PublicationsController < ApplicationController
       else
         @page = params[:page] || @a_to_z[0]
         #I'm not sure if the first condition here is the same as the authorities scope, but it might be
-        @current_objects = Publication.where("publications.id = authority_id").upper_name_like("#{@page}%").order_by_upper_name
+        @current_objects = Publication.includes(:publisher, :works).where("publications.id = authority_id").
+            upper_name_like("#{@page}%").order_by_upper_name
       end
-      
+
     end
 
     before :show do
@@ -57,7 +58,7 @@ class PublicationsController < ApplicationController
     before :new do
       #Anyone with 'editor' role (anywhere) can add publications
       permit "editor"
-      
+
       @publishers = Publisher.authorities.order_by_name
       @publications = Publication.authorities.order_by_name
     end
@@ -66,29 +67,29 @@ class PublicationsController < ApplicationController
       #Anyone with 'editor' role (anywhere) can add publications
       permit "editor"
     end
-    
+
     before :edit do
       #Anyone with 'editor' role (anywhere) can update publications
       permit "editor"
-      
+
       @publishers = Publisher.authorities.order_by_name
       @publications = Publication.order_by_name
     end
-    
+
     before :update do
       #Anyone with 'editor' role (anywhere) can update publications
       permit "editor"
     end
-   
+
   end
 
   def authorities
     #Only group editors can assign authorities
     permit "editor of Group"
-    
+
     @a_to_z = Publication.letters
     @page = params[:page] || @a_to_z[0]
-    
+
     if params[:q]
       query = params[:q]
       @current_objects = current_objects
@@ -137,17 +138,17 @@ class PublicationsController < ApplicationController
     session[:publication_auths] = @pas
     redirect_to authorities_publications_path(:page => @page) unless request.xhr?
   end
-  
+
   def update_multiple
     #Only system-wide editors can assign authorities
     permit "editor of Group"
-    
+
     pub_ids = params[:pub_ids]
     auth_id = params[:auth_id]
-    
+
     @a_to_z = Publication.letters
     @page = params[:page] || @a_to_z[0]
-    
+
     if auth_id
       update = Publication.update_multiple(pub_ids, auth_id)
       session[:publication_auths] = nil
@@ -186,18 +187,18 @@ class PublicationsController < ApplicationController
       if full_success
         flash[:notice] = "Publications were successfully deleted."
         #forward back to path which was specified in params
-        format.html {redirect_to return_path }
-        format.xml  {head :ok}
+        format.html { redirect_to return_path }
+        format.xml { head :ok }
       else
         flash[:warning] = "This publication has #{works.length} work associated with it, which must be altered or removed before this publication can be deleted."
-        format.html {redirect_to edit_publication_path(publication.id)  }
-        format.xml  {head :ok}
+        format.html { redirect_to edit_publication_path(publication.id) }
+        format.xml { head :ok }
       end
     end
   end
 
   private
-  
+
   def current_objects
     #TODO: If params[:q], handle multiple request types:
     # * ISSN
