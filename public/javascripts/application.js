da1bfe48a7bcd5982a9d2d8e789e164781a69540
Howard Ding <hding2@illinois.edu>
2011-04-22 12:03:36 -0700

    Working on redoing some javascript from new person form.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,45 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function populate_person_form(ldap_result_index, form) {
-  var f = $(form);
-  var res = ldap_results[ldap_result_index];
-
-  f['person_uid'].value = res.uid;
-  f['person_first_name'].value = res.givenname;
-  f['person_last_name'].value = res.sn;
-  if (res.middlename) {
-    f['person_middle_name'].value = res.middlename
-  }
-  if (res.generationqualifier) {
-    f['person_suffix'].value = res.generationqualifier
-  }
-  if (res.displayname) {
-    f['person_display_name'].value = res.displayname
-  }
-  if (res.postaladdress) {
-    var pa = res.postaladdress.replace(/\$/g, "\n")
-    pa = pa.replace(/\\N/g, "\n")
-    f['person_postal_address'].value = pa
-  }
-  //if (res.postaladdress) {
-  //  var aparts = res.postaladdress.split('$');
-  //  f['person_office_address_line_one'].value = aparts[0];
-  //  if (aparts[1]) { f['person_office_address_line_two'].value = aparts[1]; }
-  //}
-
-  if (res.mail) {
-    f['person_email'].value = res.mail
-  }
-  if (res.telephonenumber) {
-    f['person_phone'].value = res.telephonenumber
-  }
-
-  // Jump down to the form
-  Element.scrollTo('personal_info')
-}
-
-
 /* select_all method
  *
  * Selects/Unselects all checkboxes in a given form on a page.
@@ -429,8 +390,9 @@ function get_name_list_id(element) {
 }
 
 function decode_js_data_div(div_id) {
-  if ($(div_id))
-    return $(div_id).innerHTML.unescapeHTML().evalJSON(true);
+  div = $jq('#' + div_id)
+  if(div)
+    return $jq.parseJSON(div.text());
   else
     return null;
 }
--- public/javascripts/specific/people/new.js
+++ public/javascripts/specific/people/new.js
@@ -1,1 +1,48 @@
 var ldap_results = decode_js_data_div('ldap_results')
+
+function populate_person_form(ldap_result_index) {
+  var f = $('new_person');
+  var res = ldap_results[ldap_result_index];
+
+  f['person_uid'].value = res.uid;
+  f['person_first_name'].value = res.givenname;
+  f['person_last_name'].value = res.sn;
+  if (res.middlename) {
+    f['person_middle_name'].value = res.middlename
+  }
+  if (res.generationqualifier) {
+    f['person_suffix'].value = res.generationqualifier
+  }
+  if (res.displayname) {
+    f['person_display_name'].value = res.displayname
+  }
+  if (res.postaladdress) {
+    var pa = res.postaladdress.replace(/\$/g, "\n")
+    pa = pa.replace(/\\N/g, "\n")
+    f['person_postal_address'].value = pa
+  }
+  //if (res.postaladdress) {
+  //  var aparts = res.postaladdress.split('$');
+  //  f['person_office_address_line_one'].value = aparts[0];
+  //  if (aparts[1]) { f['person_office_address_line_two'].value = aparts[1]; }
+  //}
+
+  if (res.mail) {
+    f['person_email'].value = res.mail
+  }
+  if (res.telephonenumber) {
+    f['person_phone'].value = res.telephonenumber
+  }
+
+  // Jump down to the form
+  Element.scrollTo('personal_info')
+}
+
+$jq(function () {
+  $jq('.ldap-result input').each(function(i, e) {
+    $jq(e).click(function () {
+      populate_person_form(i);
+    })
+  })
+}
+    )
