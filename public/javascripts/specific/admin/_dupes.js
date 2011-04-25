a371df69848807b66457bcd238b1868dff37b273
Howard Ding <hding2@illinois.edu>
2011-04-25 15:51:44 -0700

    fix another typo

--- public/javascripts/specific/admin/_dupes.js
+++ public/javascripts/specific/admin/_dupes.js
@@ -2,7 +2,7 @@ $jq(function () {
   $jq('#global-checkbox').change(function () {
     jq_select_all(this, '#work-list input');
   });
-  $jq('#dupes-form').submit(function() {
+  $jq('#dupes_form').submit(function() {
     return confirm_delete_for_items_selected_by('#dupes_form input:checked');
   })
 }
