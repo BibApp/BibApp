544d3c77c1fd335bab4b13844b5b2d0deadd282d
Howard A. Ding <hading@hading.dnsalias.com>
2011-05-19 22:13:24 -0500

    Migrations for the omniauth authentications table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418180227) do
+ActiveRecord::Schema.define(:version => 20110520031023) do
 
   create_table "attachments", :force => true do |t|
     t.string   "filename"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(:version => 20110418180227) do
     t.datetime "updated_at"
     t.string   "type"
   end
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authentications", ["uid"], :name => "index_authentications_on_uid"
+  add_index "authentications", ["user_id"], :name => "index_authentications_on_user_id"
 
   create_table "contributorship_states", :force => true do |t|
     t.string "name"
