256c937a41e1d9fbc36d925d5753bae7d5af3cde
Howard Ding <hding2@illinois.edu>
2011-05-23 15:06:19 -0700

    Separated OpenID and OAuth configuration. Added concept of 'preset' open id authentications - for now these display their own button on the login page which go directly to the appropriate login page.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
-require 'omniauth/openid'
-require 'openid/store/filesystem'
 
-Bibapp::Application.config.allow_open_id = true
 Bibapp::Application.config.oauth_config =
     if File.exists?(File.join(Rails.root, 'config', 'omniauth.yml'))
       YAML.load_file(File.join(Rails.root, 'config', 'omniauth.yml'))
@@ -10,9 +7,6 @@ Bibapp::Application.config.oauth_config =
     end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Bibapp::Application.config.allow_open_id
-    provider :open_id, OpenID::Store::Filesystem.new('/tmp')
-  end
   if Bibapp::Application.config.oauth_config
     Bibapp::Application.config.oauth_config.each do |k, v|
       provider k, v['key'], v['secret']
