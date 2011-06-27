9cd5eac17e32fea338a92e0e7a81e3dcab466c63
Howard Ding <hding2@illinois.edu>
2011-06-27 11:40:52 -0700

    Form base for shibboleth url from the personalize file.

--- config/initializers/shibbolelth.rb
+++ config/initializers/shibbolelth.rb
@@ -1,9 +1,13 @@
+require 'config/personalize.rb'
+
 module OmniAuth
   module Strategies
     autoload :Shibboleth, 'lib/shibboleth_omniauth'
   end
 end
 
+#change to the appropriate location for the shibboleth provider
+shib_base = $REPOSITORY_BASE_URL.sub(/^http/, 'https')
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :shibboleth, "https://localhost:3000/Shibboleth.sso/Login"
+  provider :shibboleth, "#{shib_base}/Shibboleth.sso/Login"
 end
