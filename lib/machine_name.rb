d2d6d3e62cb35594f70e59d7ce963d8595e44003
Howard Ding <hding2@illinois.edu>
2011-07-21 15:03:22 -0700

    I think this needs to be ! - for some reason applying gsub in a console returns an enumerator, whereas applying gsub! returns the modified multibyte character object. We want the latter for chaining.

--- lib/machine_name.rb
+++ lib/machine_name.rb
@@ -4,6 +4,6 @@ module MachineName
   #  1. all punctuation/spaces converted to single space
   #  2. stripped of leading/trailing spaces and downcased
   def make_machine_name(string)
-    string.mb_chars.gsub(/[\W]+/, " ").strip.downcase
+    string.mb_chars.gsub!(/[\W]+/, " ").strip.downcase
   end
 end
