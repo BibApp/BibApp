9a48b01720b1bd8f23770e30c7960ef9d1357b46
Howard Ding <hding2@illinois.edu>
2011-07-25 15:10:29 -0700

    Refactored method.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
-  
+
   def index
     # Default BibApp search method - ApplicationController
     search(params)
-    
+
     respond_to do |format|
       format.html # Do HTML      
       format.json
@@ -11,73 +11,47 @@ class SearchController < ApplicationController
       format.xml
       format.rdf
       format.xml
-      format.rdf  
+      format.rdf
     end
   end
-  
+
   def advanced
-    if !params[:keywords].nil? || !params[:title].nil? || !params[:authors].nil? || !params[:issn_isbn].nil? || !params[:groups].nil?
-      
-      logger.debug(params.inspect)
-      # Process the params and redirect to /search
-      @q = Array.new
-      
-      logger.debug(@q.inspect)
-      # Add keywords to query
-      if !params[:keywords].nil? && !params[:keywords].empty?
-        @q << params[:keywords]
-      end
-
-      # Add title to query
-      if !params[:title].nil? && !params[:title].empty?
-        @q << "title:#{params[:title]}"
-      end
-
-      # Add author to query
-      if !params[:authors].nil? && !params[:authors].empty?
-        @q << "authors:#{params[:authors]}"
-      end
-      
-      # Add group to query
-      if !params[:groups].nil? && !params[:groups].empty?
-        @q << "groups:#{params[:groups]}"
-      end
-      
-      # Add issn_isbn to query
-      if !params[:issn_isbn].nil? && !params[:issn_isbn].empty?
-        logger.debug(params[:issn_isbn].inspect)
-        @q << "issn_isbn:#{params[:issn_isbn]}"
-      end
-      
-      # Add year to query
-      # Begin with SOLR/lucene's wildcard
-      start_date = "*"
-      end_date = "*"
-      
-      # If there is a start_date use it
-      if !params[:start_date].nil? && !params[:start_date].empty?
-        start_date = params[:start_date]
-      end
-
-      # If there is a end_date use it
-      if !params[:end_date].nil? && !params[:end_date].empty?
-        end_date = params[:end_date]
-      end
-      
-      # Only if we have a non-default start_date or end_date add it to @q
-      if start_date == "*" && end_date == "*"
-        # Do Nothing
-      else 
-        @q << "year:[#{start_date} TO #{end_date}]"
-      end
-
-      logger.debug(@q.inspect)      
-      # Redirect to /search with properly formated solr standard request params
-      redirect_to search_path(:q => @q.join(", "))
-    else
+    normal_fields = [:title, :authors, :groups, :issn_isbn]
+    fields = normal_fields << :keywords
+    if !fields.detect { |f| params[f].present? }
       @q = nil
-      # There's nothing to return, show them the search form
+      return
     end
+
+    logger.debug(params.inspect)
+    # Process the params and redirect to /search
+    @q = Array.new
+
+    logger.debug(@q.inspect)
+    # Add keywords to query
+    if !params[:keywords].nil? && !params[:keywords].empty?
+      @q << params[:keywords]
+    end
+
+    #Add 'normal' fields to query
+    normal_fields.each do |field|
+      if params[field].present?
+        @q << "#{field}:#{params[field]}"
+      end
+    end
+
+    # Add year to query
+    start_date = params[:start_date].present? ? params[:start_date] : "*"
+    end_date = params[:end_date].present? ? params[:end_date] : "*"
+
+    # Only if we have a non-default start_date or end_date add it to @q
+    unless start_date == "*" and end_date == "*"
+      @q << "year:[#{start_date} TO #{end_date}]"
+    end
+
+    logger.debug(@q.inspect)
+    # Redirect to /search with properly formated solr standard request params
+    redirect_to search_path(:q => @q.join(", "))
   end
 
 end
