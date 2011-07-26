d3ed5a0e91377a4e09515229988010cb540d72b3
Howard Ding <hding2@illinois.edu>
2011-07-26 09:22:19 -0700

    Refactoring long method. Still in progress.

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,8 +1,8 @@
 class KeywordsController < ApplicationController
-  
+
   #Require a user be logged in to create / update / destroy
-  before_filter :login_required, :only => [ :new, :create, :edit, :update, :destroy ]
-  
+  before_filter :login_required, :only => [:new, :create, :edit, :update, :destroy]
+
   make_resourceful do
     build :all
   end
@@ -22,81 +22,83 @@ class KeywordsController < ApplicationController
       last_year = facet_years.last.name
       year_arr = Range.new(first_year, last_year).to_a
     end
-    
+
     @year_keywords = Array.new
     @chart_urls = Array.new
     @work_counts = Array.new
     @years = Array.new
 
     year_arr.each do |y|
-      ydata = KeywordsHelper::YearTag.new
-      ydata.year = y
-      ydata.tags = Array.new
+      year_data = KeywordsHelper::YearTag.new
+      year_data.year = y
+      year_data.tags = Array.new
 
       params[:fq] = "year_facet:\"#{y}\""
       search(params)
 
       work_count = @q.data['response']['numFound']
-
       next if work_count == 0
-      
+
       @work_counts << work_count
       @years << y
 
-      #generate the google chart URI
-      #see http://code.google.com/apis/chart/docs/making_charts.html
-      #
-      chd = "chd=t:"
-      chl = "chl="
-      chdl = "chdl="
-      chdlp = "chdlp=b|"
-      @facets[:types].each_with_index do |r,i|
-        perc = (r.value.to_f/work_count.to_f*100).round.to_s
-        chd += "#{perc},"
-        ref = r.name.to_s == 'BookWhole' ? 'Book' : r.name.to_s
-        chl += "#{ref.titleize.pluralize}(#{r.value})|"
-        chdl += "#{perc}% #{ref.titleize.pluralize}|"
-        chdlp += "#{i.to_s},"
-      end
-      chd = chd[0...(chd.length-1)]
-      chl = chl[0...(chl.length-1)]
-      chdl = chdl[0...(chdl.length-1)]
-      chdlp = chdlp[0...(chdlp.length-1)]
+      @chart_urls << google_chart_url(work_count, @facets[:types])
 
-      if chd.nil? or chl.nil?
-        @chart_urls << "#"
-      else
-        @chart_urls << "http://chart.apis.google.com/chart?cht=p&chco=346090&chs=350x100&#{chd}&#{chl}"
-      end
-
-      #generate normalized keyword list
-      max = 25
-      bin_count = 5
-      kwords = @facets[:keywords].first(max)
-
-      if kwords.blank?
-          yt = KeywordsHelper::TagDatum.new(Struct.new(:name, :count, :year).new(nil,nil,y))
-          yt.bin = 3
-          yt.year = y
-          ydata.tags << yt
-      else
-        max_kw_freq = kwords[0].value.to_i > bin_count ? kwords[0].value.to_i : bin_count
-
-        @keywords = kwords.map { |kw|
-          s = Struct.new(:name, :count, :year)
-          s.new(kw.name, kw.value, y)
-        }.sort { |a, b| a.name <=> b.name }
-
-        @keywords.each do |kw|
-          yt = KeywordsHelper::TagDatum.new(kw)
-          yt.bin = ((kw.count.to_f * bin_count.to_f)/max_kw_freq).ceil
-          yt.year = y
-          ydata.tags << yt
-        end
-      end
-      @year_keywords << ydata unless ydata.tags.blank?
+      add_tags(year_data, @facets[:keywords])
+      @year_keywords << year_data unless year_data.tags.blank?
     end
 
   end
-  
+
+  protected
+
+  def add_tags(year_data, all_keywords)
+    #generate normalized keyword list
+    max = 25
+    bin_count = 5
+    used_keywords = all_keywords.first(max)
+
+    if used_keywords.blank?
+      yt = KeywordsHelper::TagDatum.new(Struct.new(:name, :count, :year).new(nil, nil, y))
+      yt.bin = 3
+      yt.year = y
+      year_data.tags << yt
+    else
+      max_kw_freq = used_keywords[0].value.to_i > bin_count ? used_keywords[0].value.to_i : bin_count
+
+      keyword_array = used_keywords.map { |kw|
+        s = Struct.new(:name, :count, :year)
+        s.new(kw.name, kw.value, y)
+      }.sort { |a, b| a.name <=> b.name }
+
+      keyword_array.each do |kw|
+        yt = KeywordsHelper::TagDatum.new(kw)
+        yt.bin = ((kw.count.to_f * bin_count.to_f)/max_kw_freq).ceil
+        yt.year = y
+        year_data.tags << yt
+      end
+    end
+  end
+
+  #generate the google chart URI
+  #see http://code.google.com/apis/chart/docs/making_charts.html
+  def google_chart_url(work_count, types)
+    chd = "chd=t:"
+    chl = "chl="
+    types.each_with_index do |r, i|
+      perc = (r.value.to_f/work_count.to_f*100).round.to_s
+      chd += "#{perc},"
+      ref = r.name.to_s == 'BookWhole' ? 'Book' : r.name.to_s
+      chl += "#{ref.titleize.pluralize}(#{r.value})|"
+    end
+    chd = chd[0...(chd.length-1)]
+    chl = chl[0...(chl.length-1)]
+
+    if chd and chl
+      "http://chart.apis.google.com/chart?cht=p&chco=346090&chs=350x100&#{chd}&#{chl}"
+    else
+      "#"
+    end
+  end
+
 end
