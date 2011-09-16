fe0d9c7f2a6fe69376ec9073b5237899aeeb650b
Howard Ding <hding2@illinois.edu>
2011-09-16 10:29:58 -0500

    Simplify idiom used.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -207,8 +207,8 @@ class PeopleController < ApplicationController
       chdl += "#{percent}% #{ref.titleize.pluralize}|"
       chdlp += "#{i.to_s},"
     end
-    chd = chd[0...(chd.length-1)]
-    chl = chl[0...(chl.length-1)]
+    chd.chop!
+    chl.chop!
     @chart_url = "http://chart.apis.google.com/chart?cht=p&chco=346090&chs=350x100&#{chd}&#{chl}"
 
     render :update do |page|
--- app/helpers/google_charts_helper.rb
+++ app/helpers/google_charts_helper.rb
@@ -11,8 +11,8 @@ module GoogleChartsHelper
       ref = r.name.to_s == 'BookWhole' ? 'Book' : r.name.to_s
       chl += "#{ref.titleize.pluralize}|"
     end
-    chd = chd[0...(chd.length-1)]
-    chl = chl[0...(chl.length-1)]
+    chd.chop!
+    chl.chop!
     "http://chart.apis.google.com/chart?cht=p&chco=346090&chs=350x100&#{chd}&#{chl}"
   end
 
