45a1381dba90a94845abbf3e1b15a5c22583acd7
Claudia Juergen <claudia.juergen@tu-dortmund.de>
2011-09-22 09:43:42 +0200

    i18n initial work

--- config/routes.rb
+++ config/routes.rb
@@ -1,228 +1,230 @@
 Bibapp::Application.routes.draw do
 
-  resources :works do
-    collection do
-      get :auto_complete_for_author_string
-      get :auto_complete_for_editor_string
-      get :auto_complete_for_keyword_name
-      get :auto_complete_for_publication_name
-      get :auto_complete_for_publisher_name
-      get :auto_complete_for_tag_name
-      get :orphans
-      delete :destroy_multiple
-      #Following support legacy RJS stuff
-      post :reorder_list
-      post :add_item_to_list
-      post :remove_item_from_list
-      post :add_author_to_list
-      post :remove_author_from_list
-      post :add_contributor_to_list
-      post :remove_contributor_from_list
-      post :orphans_delete
-    end
-    member do
-      get :merge_duplicates
-      get :add_to_saved
-      get :remove_from_saved
-      put :change_type
+  scope "(:locale)", :locale => /en|de/ do  
+    resources :works do
+      collection do
+        get :auto_complete_for_author_string
+        get :auto_complete_for_editor_string
+        get :auto_complete_for_keyword_name
+        get :auto_complete_for_publication_name
+        get :auto_complete_for_publisher_name
+        get :auto_complete_for_tag_name
+        get :orphans
+        delete :destroy_multiple
+        #Following support legacy RJS stuff
+        post :reorder_list
+        post :add_item_to_list
+        post :remove_item_from_list
+        post :add_author_to_list
+        post :remove_author_from_list
+        post :add_contributor_to_list
+        post :remove_contributor_from_list
+        post :orphans_delete
+      end
+      member do
+        get :merge_duplicates
+        get :add_to_saved
+        get :remove_from_saved
+        put :change_type
+      end
+
+      resources :attachments
     end
 
-    resources :attachments
-  end
-  #####
-  # Person routes
-  #####
-  resources :people do
-    collection do
-      get :batch_csv_show
-      post :batch_csv_create
-    end
-    resources :attachments
-    resources :works
-    resources :groups
-    resources :pen_names
-    resources :memberships
-    resources :roles do
+    #####
+    # Person routes
+    #####
+    resources :people do
       collection do
-        get :new_admin
-        get :new_editor
+        get :batch_csv_show
+        post :batch_csv_create
+      end
+      resources :attachments
+      resources :works
+      resources :groups
+      resources :pen_names
+      resources :memberships
+      resources :roles do
+        collection do
+          get :new_admin
+          get :new_editor
+        end
+      end
+      resources :keywords do
+        collection do
+          get :timeline
+        end
       end
     end
-    resources :keywords do
+
+    #####
+    # Group routes
+    ##### 
+    # Add Auto-Complete routes for adding new groups
+    resources :groups do
       collection do
-        get :timeline
+        get :auto_complete_for_group_name
+        get :hidden
+      end
+      member do
+        get :hide
+        get :unhide
+      end
+      resources :works
+      resources :people
+      resources :roles do
+        collection do
+          get :new_admin
+          get :new_editor
+        end
+      end
+      resources :keywords do
+        collection do
+          get :timeline
+        end
       end
     end
-  end
 
-  #####
-  # Group routes
-  ##### 
-  # Add Auto-Complete routes for adding new groups
-  resources :groups do
-    collection do
-      get :auto_complete_for_group_name
-      get :hidden
-    end
-    member do
-      get :hide
-      get :unhide
-    end
-    resources :works
-    resources :people
-    resources :roles do
+    #####
+    # Membership routes
+    #####
+    # Add Auto-Complete routes
+    resources :memberships do
       collection do
-        get :new_admin
-        get :new_editor
+        get :auto_complete_for_group_name
+        put :create_multiple
+        post :sort
+        post :search_groups
       end
     end
-    resources :keywords do
+
+    #####
+    # Contributorship routes
+    #####
+    resources :contributorships do
       collection do
-        get :timeline
+        get :admin
+        get :archivable
+        put :act_on_multiple
+      end
+
+      member do
+        put :verify
+        put :deny
       end
     end
-  end
-
-  #####
-  # Membership routes
-  #####
-  # Add Auto-Complete routes
-  resources :memberships do
-    collection do
-      get :auto_complete_for_group_name
-      put :create_multiple
-      post :sort
-      post :search_groups
-    end
-  end
-
-  #####
-  # Contributorship routes
-  #####
-  resources :contributorships do
-    collection do
-      get :admin
-      get :archivable
-      put :act_on_multiple
+    #####
+    # Publisher routes
+    #####   
+    resources :publishers do
+      collection do
+        get :authorities
+        put :update_multiple
+        get :add_to_box
+        get :remove_from_box
+      end
     end
 
-    member do
-      put :verify
-      put :deny
+    #####
+    # Publication routes
+    #####
+    resources :publications do
+      collection do
+        get :authorities
+        put :update_multiple
+        get :add_to_box
+        get :remove_from_box
+      end
     end
-  end
-  #####
-  # Publisher routes
-  #####   
-  resources :publishers do
-    collection do
-      get :authorities
-      put :update_multiple
-      get :add_to_box
-      get :remove_from_box
+
+    ####
+    # User routes
+    ####
+    # Make URLs like /user/1/password/edit for Users managing their passwords
+    resources :users do
+      resources :imports
+      resource :password
+      collection do
+        match 'activate(/:activation_code)', :to => 'users#activate', :as => 'activate'
+      end
+      member do
+        get :update_email
+      end
     end
-  end
 
-  #####
-  # Publication routes
-  #####
-  resources :publications do
-    collection do
-      get :authorities
-      put :update_multiple
-      get :add_to_box
-      get :remove_from_box
+    ####
+    # Import routes
+    ####
+    resources :imports do
+      resource :user
+      resources :attachments
     end
-  end
 
-  ####
-  # User routes
-  ####
-  # Make URLs like /user/1/password/edit for Users managing their passwords
-  resources :users do
-    resources :imports
-    resource :password
-    collection do
-      match 'activate(/:activation_code)', :to => 'users#activate', :as => 'activate'
-    end
-    member do
-      get :update_email
-    end
-  end
+    ####
+    # Search route
+    ####
+    match 'search', :to => 'search#index', :as => 'search'
+    match 'search/advanced', :to => 'search#advanced', :as => 'advanced_search'
 
-  ####
-  # Import routes
-  ####
-  resources :imports do
-    resource :user
-    resources :attachments
-  end
-
-  ####
-  # Search route
-  ####
-  match 'search', :to => 'search#index', :as => 'search'
-  match 'search/advanced', :to => 'search#advanced', :as => 'advanced_search'
-
-  ####
-  # Saved routes
-  ####
-  match 'saved', :to => 'user_sessions#saved', :as => 'saved'
-  match 'sessions/delete_saved', :to => 'user_sessions#delete_saved',
+    ####
+    # Saved routes
+    ####
+    match 'saved', :to => 'user_sessions#saved', :as => 'saved'
+    match 'sessions/delete_saved', :to => 'user_sessions#delete_saved',
         :as => 'delete_saved'
-  match 'sessions/add_many_to_saved', :to => 'user_sessions#add_many_to_saved',
+    match 'sessions/add_many_to_saved', :to => 'user_sessions#add_many_to_saved',
         :as => 'add_many_to_saved'
-  ####
-  # Authentication routes
-  ####
-  # Make easier routes for authentication (via restful_authentication)
-  match 'signup', :to => 'users#new', :as => 'signup'
-  match 'login', :to => 'user_sessions#new', :as => 'login'
-  match 'logout', :to => 'user_sessions#destroy', :as => 'logout'
-  match 'activate/:activation_code', :to => 'users#activate', :as => 'activate'
+    ####
+    # Authentication routes
+    ####
+    # Make easier routes for authentication (via restful_authentication)
+    match 'signup', :to => 'users#new', :as => 'signup'
+    match 'login', :to => 'user_sessions#new', :as => 'login'
+    match 'logout', :to => 'user_sessions#destroy', :as => 'logout'
+    match 'activate/:activation_code', :to => 'users#activate', :as => 'activate'
 
-  ####
-  # DEFAULT ROUTES 
-  ####
-  # Install the default routes as the lowest priority.
-  resources :name_strings
-  resources :memberships
-  resources :pen_names do
-    collection do
-      post :create_name_string
-      post :live_search_for_name_strings
-      post :sort
+    ####
+    # DEFAULT ROUTES 
+    ####
+    # Install the default routes as the lowest priority.
+    resources :name_strings
+    resources :memberships
+    resources :pen_names do
+      collection do
+        post :create_name_string
+        post :live_search_for_name_strings
+        post :sort
+      end
     end
-  end
-  resources :keywords
-  resources :keywordings
-  resources :passwords
-  resources :attachments do
-    collection do
-      post :add_upload_box
+    resources :keywords
+    resources :keywordings
+    resources :passwords
+    resources :attachments do
+      collection do
+        post :add_upload_box
+      end
     end
-  end
 
-  # Default homepage to works index action
-  root :to => 'works#index'
+    # Default homepage to works index action
+    root :to => 'works#index'
 
-  match 'citations', :to => 'works#index'
+    match 'citations', :to => 'works#index'
 
-  resource :user_session
+    resource :user_session
 
-  resources :authentications
-  match '/auth/:provider/callback' => 'authentications#create'
-
-  match 'admin/index'
-  match 'admin/duplicates'
-  match 'admin/ready_to_archive'
-  match 'admin/update_sherpa_data'
-  match 'admin/deposit_via_sword'
-  match 'admin/update_publishers_from_sherpa'
+    resources :authentications
+    match '/auth/:provider/callback' => 'authentications#create'
+    match '/admin/index' => "admin#index"
+    match 'admin/duplicates' => "admin#duplicates"
+    match 'admin/ready_to_archive' => "admin#ready_to_archive"
+    match 'admin/update_sherpa_data' => "admin#update_sherpa_data"
+    match 'admin/deposit_via_sword' => "admin#deposit_via_sword"
+    match 'admin/update_publishers_from_sherpa' => "admin#update_publishers_from_sherpa"
   
-  match 'roles/index'
-  match 'roles/destroy'
-  match 'roles/create'
-  match 'roles/new_admin'
-  match 'roles/new_editor'
+    match 'roles/index' => "roles#index"
+    match 'roles/destroy' => "roles#destroy"
+    match 'roles/create' => "roles#create"
+    match 'roles/new_admin' => "roles#new_admin"
+    match 'roles/new_editor' => "roles#new_editor"
+  end
 end
