17b5615238ce9bb22799e136df3830560bbc8d04
Howard Ding <hading2@gmail.com>
2011-09-23 22:26:42 -0500

    Refactor more views.

--- app/helpers/name_strings_helper.rb
+++ app/helpers/name_strings_helper.rb
@@ -1,2 +1,6 @@
 module NameStringsHelper
+  def person_image(person)
+    image_tag(person.image_url, :class => 'person-photo', :size => "75x100",
+              :alt => person.display_name, :title => person.display_name)
+  end
 end
