0522946c91b2db537d318396afad2e02d3d0bde7
Howard Ding <hding2@illinois.edu>
2011-09-26 11:00:30 -0500

    extract logic to helper minor refactoring

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,4 +2,9 @@ module GroupsHelper
   def body_header(group)
     (group.name + link_to(image_tag("feed-icon-14x14.png"), group_path(@group, :format => "rss"))).html_safe
   end
+
+  def parent_link(group)
+    group.parent ? link_to(group.parent.name, group.parent) : ''
+  end
+
 end
