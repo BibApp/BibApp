340238a055e69c7adead3565c38b0d01843e73a8
Howard Ding <hding2@illinois.edu>
2011-09-27 17:31:42 -0500

    Add SharedHelper to contain methods for shared views only. Since we don't know what uses these views, include this helper into all views in ApplicationController. The distinction between this and ApplicationHelper is that methods in here are really intended only to be used by the shared views, not arbitrary views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   clear_helpers
   helper PrototypeHelper
   helper AutoCompleteMacrosHelper
+  helper SharedHelper
   helper_method :current_user_session, :current_user, :logged_in?
 
   # See ActionController::RequestForgeryProtection for details
---
+++ app/helpers/shared_helper.rb
@@ -0,0 +1,7 @@
+#this helper is for views in the shared folder
+#since we don't know from whence they are called this is included into all views by ApplicationController -
+#nevertheless, I want the separation from the methods in ApplicationHelper - these are really specific to
+#the shared views, not all views
+module SharedHelper
+
+end
