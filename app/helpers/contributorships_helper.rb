6aa77a80113929306014b3fc9be9d4ac49d4960b
Howard Ding <hading2@gmail.com>
2011-10-04 20:11:08 -0500

    i18n for contributorships controller and helper

--- app/helpers/contributorships_helper.rb
+++ app/helpers/contributorships_helper.rb
@@ -1,15 +1,17 @@
 module ContributorshipsHelper
   def body_header(person, status)
-    "#{link_to(h(person.display_name), person_path(person))}: #{status.capitalize} Contributorships".html_safe
+    t('common.contributorships.index_title', :display_name => link_to(h(person.display_name), person_path(person)),
+      :status => t("common.contributorships.#{status}").capitalize).html_safe
   end
 
   def status_link(person, status, link_status)
     link_to_unless((status.downcase == link_status),
-                   "#{link_status.capitalize} (#{person.contributorships.send(link_status).size})",
+                   "#{t("common.contributorships.#{link_status}")} (#{person.contributorships.send(link_status).size})",
                    contributorships_path(:person_id => person.id, :status => link_status, :page => 1))
   end
 
   def romeo_color(row)
-    row.color.present? ? row.color.downcase : 'unknown'
+    row.color.present? ? t("common.romeo.#{row.color.downcase}") : t('app.unknown')
   end
+
 end
