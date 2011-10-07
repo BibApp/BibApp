cc987c46f61953b5814bb5dc0fa7e79df46c38cf
Howard Ding <hding2@illinois.edu>
2011-10-07 11:03:10 -0500

    extracted strings

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -2,22 +2,23 @@ module RolesHelper
 
   def index_header(authorizable)
     if authorizable == System
-      "System-wide Roles: #{$APPLICATION_NAME}"
+      t('common.roles.index_header_system', :app_name => $APPLICATION_NAME)
     elsif authorizable.kind_of?(Group)
-      "Roles for Group: #{group_link(authorizable)}".html_safe
+      t('common.roles.index_header_group', group_link(authorizable)).html_safe
     elsif @authorizable.kind_of?(Person)
-      "Roles for Person: #{person_link(authorizable)}".html_safe
+      t('common.roles.index_header_person', person_link(authorizable)).html_safe
     end
   end
 
   def form_header(authorizable, role_name)
-    if authorizable.is_a? Class and authorizable==System
-      "Add System-wide #{role_name}: #{$APPLICATION_NAME}"
+    translated_role = t("common.roles.#{role_name.downcase}")
+    if authorizable.is_a? Class and authorizable == System
+      t('common.roles.form_header_system', :role => translated_role, :app_name => $APPLICATION_NAME)
     elsif authorizable.kind_of?(Group)
-      "Add #{role_name} on Group: #{group_link(authorizable)}".html_safe
+      t('common.roles.form_header_group', :role => translated_role, :group => group_link(authorizable))
     elsif @authorizable.kind_of?(Person)
-      "Add #{role_name} on Person: #{person_link(authorizable)}".html_safe
-    end
+      t('common.roles.form_header_person', :role => translated_role, :person => person_link(authorizable))
+    end.html_safe
   end
 
   def group_link(group)
