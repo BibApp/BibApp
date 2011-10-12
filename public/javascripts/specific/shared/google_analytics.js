23759dd17ae454b4f7d3a2ad91cbe27c81d741d9
Howard Ding <hding2@illinois.edu>
2011-10-12 09:22:09 -0500

    Updated Google Analytics code to use new Asynchronous version. Merge of jstirnaman code. Conflicts:
    	.project
    	app/views/layouts/application.html.haml
    	public/javascripts/specific/shared/google_analytics.js

--- public/javascripts/specific/shared/google_analytics.js
+++ public/javascripts/specific/shared/google_analytics.js
@@ -1,8 +1,9 @@
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-try {
-  var googleAnalyticsId = decode_js_data_div('google-analytics-id');
-  var pageTracker = _gat._getTracker(googleAnalyticsId);
-  pageTracker._trackPageview();
-} catch(err) {
-}
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', googleAnalyticsId]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
