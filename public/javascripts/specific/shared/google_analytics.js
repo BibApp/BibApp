2bc47958add8009ad48cbb8385305dda3570bb6a
Howard Ding <hding2@illinois.edu>
2011-10-12 09:16:59 -0500

    add missing ;

--- public/javascripts/specific/shared/google_analytics.js
+++ public/javascripts/specific/shared/google_analytics.js
@@ -1,7 +1,7 @@
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 try {
-  var googleAnalyticsId = decode_js_data_div('google-analytics-id')
+  var googleAnalyticsId = decode_js_data_div('google-analytics-id');
   var pageTracker = _gat._getTracker(googleAnalyticsId);
   pageTracker._trackPageview();
 } catch(err) {
