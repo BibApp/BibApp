55fe9582015350e77a7db9c2622b18ce1372cd9f
Howard Ding <hding2@illinois.edu>
2011-10-19 15:08:33 -0500

    Translate label names. Note that this will result in some of them changing from their previous values, but this seems inevitable here.

--- app/helpers/google_charts_helper.rb
+++ app/helpers/google_charts_helper.rb
@@ -8,12 +8,19 @@ module GoogleChartsHelper
     facets[:types].each do |r|
       percent = (r.value.to_f / work_count.to_f * 100).round.to_s
       chd += "#{percent},"
-      ref = r.name.to_s == 'BookWhole' ? 'Book' : r.name.to_s
-      chl += "#{ref.titleize.pluralize}|"
+      chl += "#{chart_label(r)}|"
     end
     chd.chop!
     chl.chop!
     "http://chart.apis.google.com/chart?cht=p&chco=346090&chs=350x100&#{chd}&#{chl}"
+  end
+
+  protected
+
+  #need to translate chart labels
+  def chart_label(r)
+    name = r.name
+    name.titlecase.gsub(' ', '').constantize.model_name.human_pl
   end
 
 end
