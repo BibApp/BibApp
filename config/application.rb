41490e5b5f40747be55ba79eabda3113f75b05a7
Howard Ding <hding2@illinois.edu>
2011-10-20 13:26:27 -0500

    If there is a config/locales.yml file then read permitted locales from it. If not use :en only. Add example for locales.yml. Git ignore actual locales.yml file.

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module Bibapp
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # Specify desired locales in config/locales.yml. If that doesn't exist use English only.
+    config.i18n.available_locales = YAML.load_file(File.join(Rails.root, 'config', 'locales.yml')) rescue [:en]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
