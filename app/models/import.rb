0b13521bebb56419b0d19994d122309fe783e259
Howard Ding <hding2@illinois.edu>
2011-12-07 15:37:32 -0600

    remove lock - I think this is okay and we were getting timeouts with it.

--- app/models/import.rb
+++ app/models/import.rb
@@ -46,176 +46,173 @@ class Import < ActiveRecord::Base
   def notify_user
     logger.debug("\n=== Notify User - #{self.id} ===\n\n\n")
     current_locale = I18n.locale
-    Thread.exclusive do
+    I18n.locale = self.user.default_locale
+    Notifier.import_review_notification(self).deliver
+    I18n.locale = current_locale
+  end
+end
+
+def accept_import
+  self.delay.process_accepted_import
+end
+
+def process_accepted_import
+  logger.debug("\n=== Accepted Import - #{self.id} ===\n\n")
+  works = Work.where("id in (?)", self.works_added)
+
+  # Create unverified contributorships for each non-duplicate work
+  works.each { |w| w.create_contributorships }
+
+  # If import was for a Person, auto-verify the contributorships
+  if self.person_id
+    person = Person.find(person_id)
+    logger.debug("\n\n\n* Auto-verify contributorships - #{self.person_id}\n\n")
+
+    # Find Contributorships, set to verified.
+    contributorships = works.collect do |work|
+      Contributorship.find_or_create_by_work_id_and_person_id(work.id, self.person_id)
+    end
+
+    contributorships.each do |c|
+      c.verify_contributorship
+      c.work.set_for_index_and_save
+    end
+
+    logger.debug("\n\n\n* Batch indexing import - #{self.person_id}\n\n")
+    Index.batch_index
+
+    #Delayed Job - Update scoring hash for Person
+    person.delay.queue_update_scoring_hash
+  end
+end
+
+def reject_import
+  logger.debug("\n=== Rejected Import - #{self.id} ===\n\n")
+  logger.debug("\n* Destroying Import Works")
+
+  self.works_added.each do |work_added|
+    work = Work.find_by_id(work_added)
+    logger.debug("\n- Work: #{work.id}") if work
+    work.delay.destroy if work
+  end
+
+  self.works_added = []
+end
+
+###
+# ===== Import Object Methods =====
+###
+
+# Add Import to Delayed Job queue
+def queue_import
+  self.delay.batch_import
+end
+
+# Process Batch Import
+def batch_import
+  return unless self.state == 'processing'
+  self.transaction do
+    logger.debug("\n\n==== Starting Batch Import ==== \n\n")
+
+    # Initialize an array of all the works added and hash of errors encountered in the batch
+    self.works_added = Array.new
+    self.import_errors = Hash.new
+
+    # Init: Parser and Importer
+    citation_parser = CitationParser.new
+    citation_importer = CitationImporter.new
+
+    # Step: 1 -- Read the data
+    begin
       begin
-        I18n.locale = self.user.default_locale
-        Notifier.import_review_notification(self).deliver
-      ensure
-        I18n.locale = current_locale
-      end
-    end
-  end
-
-  def accept_import
-    self.delay.process_accepted_import
-  end
-
-  def process_accepted_import
-    logger.debug("\n=== Accepted Import - #{self.id} ===\n\n")
-    works = Work.where("id in (?)", self.works_added)
-
-    # Create unverified contributorships for each non-duplicate work
-    works.each { |w| w.create_contributorships }
-
-    # If import was for a Person, auto-verify the contributorships
-    if self.person_id
-      person = Person.find(person_id)
-      logger.debug("\n\n\n* Auto-verify contributorships - #{self.person_id}\n\n")
-
-      # Find Contributorships, set to verified.
-      contributorships = works.collect do |work|
-        Contributorship.find_or_create_by_work_id_and_person_id(work.id, self.person_id)
+        str = StringMethods.ensure_utf8(self.read_import_file)
+      rescue EncodingException => e
+        self.import_errors[:invalid_file_format] = "Citations could not be parsed as the character encoding could not be determined or could not be converted to UTF-8."
+        raise e
       end
 
-      contributorships.each do |c|
-        c.verify_contributorship
-        c.work.set_for_index_and_save
-      end
-
-      logger.debug("\n\n\n* Batch indexing import - #{self.person_id}\n\n")
-      Index.batch_index
-
-      #Delayed Job - Update scoring hash for Person
-      person.delay.queue_update_scoring_hash
-    end
-  end
-
-  def reject_import
-    logger.debug("\n=== Rejected Import - #{self.id} ===\n\n")
-    logger.debug("\n* Destroying Import Works")
-
-    self.works_added.each do |work_added|
-      work = Work.find_by_id(work_added)
-      logger.debug("\n- Work: #{work.id}") if work
-      work.delay.destroy if work
+      parsed_citations = citation_parser.parse(str)
+    rescue Exception => e
+      self.import_errors[:exception] = e
+      self.save_and_review!
+      return
     end
 
-    self.works_added = []
-  end
-
-  ###
-  # ===== Import Object Methods =====
-  ###
-
-  # Add Import to Delayed Job queue
-  def queue_import
-    self.delay.batch_import
-  end
-
-  # Process Batch Import
-  def batch_import
-    return unless self.state == 'processing'
-    self.transaction do
-      logger.debug("\n\n==== Starting Batch Import ==== \n\n")
-
-      # Initialize an array of all the works added and hash of errors encountered in the batch
-      self.works_added = Array.new
-      self.import_errors = Hash.new
-
-      # Init: Parser and Importer
-      citation_parser = CitationParser.new
-      citation_importer = CitationImporter.new
-
-      # Step: 1 -- Read the data
-      begin
-        begin
-          str = StringMethods.ensure_utf8(self.read_import_file)
-        rescue EncodingException => e
-          self.import_errors[:invalid_file_format] = "Citations could not be parsed as the character encoding could not be determined or could not be converted to UTF-8."
-          raise e
-        end
-
-        parsed_citations = citation_parser.parse(str)
-      rescue Exception => e
-        self.import_errors[:exception] = e
-        self.save_and_review!
-        return
-      end
-
-      if parsed_citations.blank?
-        self.import_errors[:no_parsed_citations] = <<-MESSAGE
+    if parsed_citations.blank?
+      self.import_errors[:no_parsed_citations] = <<-MESSAGE
         The format of the input was unrecognized or unsupported.
         <br/><strong>Supported formats include:</strong> RIS, MedLine and Refworks XML.<br/>
         In addition, if you are uploading a text file, it should use UTF-8 character encoding.
-        MESSAGE
-        self.save_and_review!
-        return
-      end
-
-      begin
-        #import citations
-        attr_hashes = citation_importer.citation_attribute_hashes(parsed_citations)
-
-        # Make sure there is data in the Attribute Hash
-        return nil if attr_hashes.nil?
-
-        #create works and reindex
-        create_works_from_attribute_hashes(attr_hashes)
-        Index.batch_index
-
-      rescue Exception => e
-        self.import_errors[:exception] = e.message
-      end
-
-      # At this point, some or all of the works were saved to the database successfully.
+      MESSAGE
       self.save_and_review!
+      return
     end
-  end
 
-  def save_and_review!
-    self.save
-    self.review!
-  end
+    begin
+      #import citations
+      attr_hashes = citation_importer.citation_attribute_hashes(parsed_citations)
 
-  def read_import_file
-    File.read("#{Rails.root}/public#{self.import_file.public_filename}")
-  end
+      # Make sure there is data in the Attribute Hash
+      return nil if attr_hashes.nil?
 
-  # Create works in database. Use a transaction to rollback if there is an error, allowing error to propagate
-  def create_works_from_attribute_hashes(attr_hashes)
-    self.transaction do
-      attr_hashes.each do |h|
-        begin
-          work = Work.create_from_hash(h, false)
-          if work.errors.blank?
-            #add to batch of works created
-            self.works_added << work.id
-          else #validation problem
-            self.import_errors[:import_error] ||= Array.new
-            self.import_errors[:import_error] << "<em>#{h[:title_primary]}</em> could not be imported. #{work.errors.to_s}<br/>"
-          end
-        rescue Exception => e
-          #actual exception
+      #create works and reindex
+      create_works_from_attribute_hashes(attr_hashes)
+      Index.batch_index
+
+    rescue Exception => e
+      self.import_errors[:exception] = e.message
+    end
+
+    # At this point, some or all of the works were saved to the database successfully.
+    self.save_and_review!
+  end
+end
+
+def save_and_review!
+  self.save
+  self.review!
+end
+
+def read_import_file
+  File.read("#{Rails.root}/public#{self.import_file.public_filename}")
+end
+
+# Create works in database. Use a transaction to rollback if there is an error, allowing error to propagate
+def create_works_from_attribute_hashes(attr_hashes)
+  self.transaction do
+    attr_hashes.each do |h|
+      begin
+        work = Work.create_from_hash(h, false)
+        if work.errors.blank?
+          #add to batch of works created
+          self.works_added << work.id
+        else #validation problem
           self.import_errors[:import_error] ||= Array.new
-          self.import_errors[:import_error] << "<em>#{h[:title_primary]}</em> could not be imported. #{e.to_s}<br/>"
+          self.import_errors[:import_error] << "<em>#{h[:title_primary]}</em> could not be imported. #{work.errors.to_s}<br/>"
         end
+      rescue Exception => e
+        #actual exception
+        self.import_errors[:import_error] ||= Array.new
+        self.import_errors[:import_error] << "<em>#{h[:title_primary]}</em> could not be imported. #{e.to_s}<br/>"
       end
     end
   end
+end
 
-  def name_string_work_count
-    works = self.works_added.collect { |work_id| Work.find_by_id(work_id) }.compact
+def name_string_work_count
+  works = self.works_added.collect { |work_id| Work.find_by_id(work_id) }.compact
 
-    # Initialize hash of name_strings
-    name_strings = Hash.new
+  # Initialize hash of name_strings
+  name_strings = Hash.new
 
-    works.each do |work|
-      work.name_strings.each do |ns|
-        name_strings[ns.name] ||= {:id => ns.id, :works => []}
-        name_strings[ns.name][:works] << work.id
-      end
+  works.each do |work|
+    work.name_strings.each do |ns|
+      name_strings[ns.name] ||= {:id => ns.id, :works => []}
+      name_strings[ns.name][:works] << work.id
     end
-
-    return name_strings
   end
+
+  return name_strings
+end
+
 end
