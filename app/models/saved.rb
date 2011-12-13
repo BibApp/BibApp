e664f9038f51b3dd6e0060fe71b6b3a851260a0d
Howard Ding <hding2@illinois.edu>
2011-12-13 16:21:11 -0600

    Made this inherit from Object - it doesn't need to be an ActiveRecord::BaseWithoutTable for any reason.

--- app/models/saved.rb
+++ app/models/saved.rb
@@ -1,16 +1,16 @@
 # Saved model captures session-based work.ids as "items", allowing users
-# to create ad-hoc lists of works for export 
-class Saved < ActiveRecord::BaseWithoutTable
+# to create ad-hoc lists of works for export
+class Saved
   attr_reader :items
-  
+
   def initialize
     @items = []
   end
-  
+
   def add_work(work)
-    @items << work.id unless @items.member?(work)
+    @items << work.id unless @items.member?(work.id)
   end
-  
+
   def remove_work(work_id)
     @items.delete(work_id)
   end
