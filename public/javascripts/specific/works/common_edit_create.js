f271f4c69d302586c5558059b5f4771b7651332d
Howard Ding <hding2@illinois.edu>
2012-01-05 12:13:20 -0600

    Javascript for adding more author/contributor rows
    Instead of going to the server we simply, upon page load, use the first item in each list to create
    a template. We then copy and insert this on demand.

--- public/javascripts/specific/works/common_edit_create.js
+++ public/javascripts/specific/works/common_edit_create.js
@@ -1,3 +1,32 @@
 function remove_enclosing_list_item(element) {
   $jq(element).closest('li').remove();
 }
+
+var author_list_item_template = null;
+function store_author_list_item_template() {
+  author_list_item_template = $jq('#author_name_strings_list li').first().clone();
+  author_list_item_template.children('.text').attr('value', '');
+}
+
+var contributor_list_item_template = null;
+function store_contributor_list_item_template() {
+  contributor_list_item_template = $jq('#contributor_name_strings_list li').first().clone();
+  contributor_list_item_template.children('.text').attr('value', '');
+}
+
+function add_author_list_item() {
+  var new_item = author_list_item_template.clone();
+  new_item.attr('id', 'author_' + $jq('#author_name_strings_list li').length);
+  $jq('#author_name_strings_list').append(new_item);
+}
+
+function add_contributor_list_item() {
+  var new_item = contributor_list_item_template.clone();
+  new_item.attr('id', 'contributor_' + $jq('#contributor_name_strings_list li').length);
+  $jq('#contributor_name_strings_list').append(new_item);
+}
+
+$jq(function() {
+  store_author_list_item_template();
+  store_contributor_list_item_template();
+})
