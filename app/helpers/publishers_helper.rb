a2277c2cd19cd3461d9a55c9f2422df3c831c03f
Howard Ding <hding2@illinois.edu>
2012-03-01 15:53:44 -0600

    Lookup most recent years and publication counts for the table en-masse. This improves the speed quite a bit.

--- app/helpers/publishers_helper.rb
+++ app/helpers/publishers_helper.rb
@@ -1,5 +1,17 @@
 module PublishersHelper
   def sherpa_colors
-    I18n.t('personalize.sherpa_colors').keys.collect {|k| k.to_s}.sort
+    I18n.t('personalize.sherpa_colors').keys.collect { |k| k.to_s }.sort
+  end
+
+  def most_recent_year_hash(publishers)
+    with_ids_from(publishers) do |ids|
+      Work.where(:publisher_id => ids).group(:publisher_id).maximum(:publication_date_year)
+    end
+  end
+
+  def publication_count_hash(publishers)
+    with_ids_from(publishers) do |ids|
+      Publication.where(:publisher_id => ids).group(:publisher_id).count
+    end
   end
 end
