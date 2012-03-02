3e7b6935fc553ee9b8d7f6f0295fcdee048e6419
Howard Ding <hding2@illinois.edu>
2012-03-02 17:25:42 -0600

    Trying one-fetch for publisher data - generating this view is still really slow on production.

--- app/helpers/publications_helper.rb
+++ app/helpers/publications_helper.rb
@@ -5,4 +5,15 @@ module PublicationsHelper
       Work.joins(:contributorships).verified.where(:publication_id => ids).group(:publication_id).select("distinct(works.id)").count
     end
   end
+
+  def publisher_data_hash(publications)
+    with_ids_from(publications) do |ids|
+      Publication.where(:id => ids).joins(:publisher).
+          select('publications.id as id, publishers.id as publ_id, publishers.name as publ_name, publishers.romeo_color as publ_color').
+          each_with_object({}) do |pub, hash|
+        hash[pub.id] = {:id => pub.publ_id, :name => pub.publ_name, :color => pub.publ_color}
+      end
+    end
+  end
+
 end
