257e6af4f40e977a1af62d7ed8692756a68098ff
Howard Ding <hding2@illinois.edu>
2012-03-07 11:52:14 -0600

    fix bug - when an object is destroyed then nothing about it will be marked as changed (i.e. obj.changed? = false), but we still want to act. So check also if it is destroyed. I'll refactor this presently.
    fix bug - if a publication is destroyed then we need to make the list of related publications a bit differently, since we can no longer look them up from active record.

--- app/sweepers/publications_sweeper.rb
+++ app/sweepers/publications_sweeper.rb
@@ -17,13 +17,13 @@ class PublicationsSweeper < AbstractSweeper
 
   def expire_content(record)
     ids = get_publication_ids(record)
-    publications = Publication.find(ids)
+    publications = (record.destroyed? and record.is_a?(Publication)) ? [record] : Publication.find(ids)
     #expire individual publication rows
     ids.each do |id|
       bibapp_expire_fragment_all_locales(:controller => 'publications', :action => 'index', :id => id, :action_suffix => 'publication-row')
     end
     #expire all relevant full index tables
-    publications.collect { |p| p.sort_name.first.upcase rescue nil }.compact.uniq.each do |page|
+    publications.collect { |p| p.sort_name.first.upcase }.compact.uniq.each do |page|
       bibapp_expire_fragment_all_locales(:controller => 'publications', :action => 'index', :page => page, :action_suffix => 'index-table')
     end
   end
@@ -31,13 +31,13 @@ class PublicationsSweeper < AbstractSweeper
   def get_publication_ids(record)
     case record
       when Work
-        record.publication_id_changed? ? [record.publication_id, record.publication_id_was].compact  : []
+        (record.destroyed? or record.publication_id_changed?) ? [record.publication_id, record.publication_id_was].compact  : []
       when Publisher
-        (record.name_changed? or record.romeo_color_changed?) ? record.publication_ids : []
+        (record.destroyed? or record.name_changed? or record.romeo_color_changed?) ? record.publication_ids : []
       when Publication
-        (record.name_changed? or record.issn_isbn_changed?) ? [record.id] : []
+        (record.destroyed? or record.name_changed? or record.issn_isbn_changed?) ? [record.id] : []
       when Contributorship
-        record.contributorship_state_id_changed? ? [record.work.publication_id] : []
+        (record.destroyed? or record.contributorship_state_id_changed?) ? [record.work.publication_id] : []
     end
   end
 end
