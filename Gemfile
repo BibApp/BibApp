09897a1cea15e065348f95ce5108da6cba15dbbc
Howard Ding <hding2@illinois.edu>
2012-03-12 15:19:41 -0500

    Use Nokogiri instead of Hpricot to parse citations.

--- Gemfile
+++ Gemfile
@@ -22,9 +22,6 @@ gem 'make_resourceful'
 #file attachment - to replace attachment_fu
 gem 'paperclip'
 
-#Hpricot - used for various HTML parsing purposes
-gem "hpricot", "~>0.6"
-
 #HTMLEntities - used to encode UTF-8 data so that it is valid in HTML
 gem "htmlentities", "~>4.0.0"
 
--- vendor/plugins/citation_parser/lib/base_xml_parser.rb
+++ vendor/plugins/citation_parser/lib/base_xml_parser.rb
@@ -1,24 +1,16 @@
 #
 # Base XML format parser
-# 
+#
 # Provides basic utilities that any XML-based
 # parser would want access to.
-# 
-# XML parsing is handled by Hpricot:
-#   http://code.whytheluckystiff.net/hpricot/
-#  
+#
+#
 class BaseXmlParser < CitationParser
-  require 'hpricot' if defined? Hpricot
-  require 'htmlentities' if defined? HTMLEntities
- 
+
   def initialize()
     super
-    
-    #initialize Hpricot's buffer to a larger value
-    # (i.e. attempt to avoid the "Hpricot::ParseError - ran out of buffer space")
-    Hpricot.buffer_size = 204800
   end
-  
+
   #Decode any XML entities after they've been parsed into a Hash.
   #  For example, this performs the following translations:
   #    &apos; => '
@@ -28,20 +20,20 @@ class BaseXmlParser < CitationParser
   #    &lt; => <
   def decode_xml_entities(props)
     coder = HTMLEntities.new
-    
+
     #decode XML entities in any value
     props.each do |key, value|
       next if key==:original_data #never decode the original citation...keep it as is!
-       
+
       #Determine whether we are cleaning arrays or strings
       if value.class.to_s=="Array" and !value.empty?
         #decode every value
-        value.collect! {|v| coder.decode(v).mb_chars}
+        value.collect! { |v| coder.decode(v).mb_chars }
       elsif (value.class.to_s=="String" or value.class.to_s=="ActiveSupport::Multibyte::Chars") and !value.empty?
         props[key] = coder.decode(value)
       end
-     end
-    
-     return props
+    end
+
+    return props
   end
 end
