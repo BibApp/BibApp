30420080807225fc2e03bf517e3ad1333120f12e
Howard Ding <hding2@illinois.edu>
2012-06-22 15:54:33 -0500

    Add more comments about specs made pending. Alter the string encoding converter a bit. Fix ruby 1.8 -> 1.9 bug (array.to_s works differently) and add brute force converter when CMESS can't do anything.

--- lib/string_methods.rb
+++ lib/string_methods.rb
@@ -13,11 +13,28 @@ module StringMethods
     encoding = CMess::GuessEncoding::Automatic.guess(str)
     # As long as encoding could be guessed, try to convert to UTF-8
     unless encoding.blank? or (encoding == CMess::GuessEncoding::Encoding::UNKNOWN)
-      return Iconv.iconv('UTF-8', encoding, str).to_s
+      return Iconv.iconv('UTF-8', encoding, str).join
     else
-      raise EncodingException
+      Rails.logger.error "Unable to convert string encoding with #{encoding} guess by CMess - trying brute force"
+      return convert_string(str)
     end
+  end
 
+  #This is an example of another way we might approach this.
+  def convert_string(str)
+    #TODO might want to use a cut-down or reordered list here
+    Encoding.list.each do |encoding|
+      begin
+        s = str.clone
+        s.force_encoding(encoding)
+        if s.valid_encoding?
+          return s.encode('UTF-8', encoding)
+        end
+      rescue
+        #do nothing - let it go to next candidate
+      end
+    end
+    raise EncodingException, "Could not convert to any encoding: #{str}"
   end
 
 end
