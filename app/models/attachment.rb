7726b23fb04e23d085ffd44cfd710c759ae9f8c8
Howard Ding <hding2@illinois.edu>
2012-06-25 13:57:45 -0500

    add comment

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,6 +4,8 @@ class Attachment < ActiveRecord::Base
   #### Associations ####
   belongs_to :asset, :polymorphic => true # Polymorphism!
 
+  #If we later want to revert to the default paperclip 3 style, change :id to :id_partition and move
+  #the assets from abcdefghi to abc/def/ghi under data.
   has_attached_file :data, :url => '/system/data/:id/:style/:basename.:extension'
   validates_attachment_size :data, :in => 1.byte...100.megabytes
   validates_attachment_presence :data
