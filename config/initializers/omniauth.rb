1fe9b209c4e0191bfc844950a219107d7eedec97
Howard Ding <hding2@illinois.edu>
2013-03-18 11:16:29 -0500

    Copy config from medusa project - we'll probably need to modify this, but it's a start.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     #enter your Omniauth strategies here
-    
+    opts = YAML.load_file(File.join(Rails.root, 'config', 'shibboleth.yml'))[Rails.env]
+    provider :shibboleth, opts.symbolize_keys
+    MedusaRails3::Application.shibboleth_host = opts['host']
   else
     provider :developer
   end
