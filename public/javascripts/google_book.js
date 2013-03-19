94278055602f0192e9cf787366746f9abd3e1b53
Howard Ding <hding2@illinois.edu>
2013-03-19 15:51:40 -0500

    reorganized so the ajax should work. I think.

--- public/javascripts/google_book.js
+++ public/javascripts/google_book.js
@@ -12,20 +12,21 @@ function get_google_links() {
 
 function get_google_link(span) {
   var isbn = $jq(span).attr('title');
-  //lookup stuff
-  var urls = lookup_google_links(isbn)
-  var image = document.createElement('img');
-  $jq(image).attr('src', urls['image_url']);
-  var link = document.createElement('a');
-  $jq(link).attr('href', urls['link_url']);
-  $jq(link).append(image);
-  $jq(span).append(link);
+  var query_url = 'https://www.googleapis.com/books/v1/volumes';
+  $jq.ajax({url: query_url,
+    data: {q: 'isbn:' + isbn}}).done(function (data) {
+        insert_google_link(data, span);
+      })
 }
 
-function lookup_google_links(isbn) {
-  var hash = {
-    link_url: 'http://books.google.com/books?id=0JA_uAAACAAJ&dq=isbn:9780470189481&hl=&cd=1&source=gbs_api',
-    image_url: 'http://bks3.books.google.com/books?id=0JA_uAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'
-  };
-  return hash;
+function insert_google_link(data, span) {
+  var volume_info = data['items'][0]['volumeInfo'];
+  var link_url = volume_info['previewLink'];
+  var image_url = volume_info['imageLinks']['smallThumbnail'];
+  var image = document.createElement('img');
+  $jq(image).attr('src', image_url);
+  var link = document.createElement('a');
+  $jq(link).attr('href', link_url);
+  $jq(link).append(image);
+  $jq(span).append(link);
 }
