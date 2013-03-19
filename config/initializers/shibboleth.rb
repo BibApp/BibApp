4211ffb2f5db7519add5ccf14702705da516ebbe
Howard Ding <hding2@illinois.edu>
2013-03-19 10:50:23 -0500

    I don't think this is needed now.

--- config/initializers/shibboleth.rb
+++ config/initializers/shibboleth.rb
@@ -1,13 +1,13 @@
-require 'config/personalize'
+#require 'config/personalize'
 
-module OmniAuth
-  module Strategies
-    autoload :Shibboleth, 'lib/shibboleth_omniauth'
-  end
-end
+#module OmniAuth
+#  module Strategies
+#    autoload :Shibboleth, 'lib/shibboleth_omniauth'
+#  end
+#end
 
 #change to the appropriate location for the shibboleth provider
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :shibboleth, "#{$APPLICATION_URL}Shibboleth.sso/Login", 'urn:mace:incommon:uiuc.edu'
-end
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :shibboleth, "#{$APPLICATION_URL}Shibboleth.sso/Login", 'urn:mace:incommon:uiuc.edu'
+#end
 
