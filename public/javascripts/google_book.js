b3a37a58c946ff6dbbd832535db5c18c6bb09d8f
Howard Ding <hding2@illinois.edu>
2013-03-19 15:31:47 -0500

    Fix bug. Refactor a bit.

--- public/javascripts/google_book.js
+++ public/javascripts/google_book.js
@@ -11,14 +11,21 @@ function get_google_links() {
 }
 
 function get_google_link(span) {
-  var isbn = $jq(span.attr('title'));
+  var isbn = $jq(span).attr('title');
   //lookup stuff
-  var link_url = 'http://books.google.com/books?id=0JA_uAAACAAJ&dq=isbn:9780470189481&hl=&cd=1&source=gbs_api';
-  var image_url = 'http://bks3.books.google.com/books?id=0JA_uAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api';
+  var urls = lookup_google_links(isbn)
   var image = document.createElement('img');
-  $jq(image).attr('src', image_url);
+  $jq(image).attr('src', urls['link_url']);
   var link = document.createElement('a');
-  $jq(link).attr('href', link_url);
+  $jq(link).attr('href', urls['image_url']);
   $jq(link).append(image);
   $jq(span).append(link);
+}
+
+function lookup_google_links(isbn) {
+  var hash = {
+    link_url: 'http://books.google.com/books?id=0JA_uAAACAAJ&dq=isbn:9780470189481&hl=&cd=1&source=gbs_api',
+    image_url: 'http://bks3.books.google.com/books?id=0JA_uAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api'
+  };
+  return hash;
 }
