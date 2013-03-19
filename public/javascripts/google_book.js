8c035a02fddc6b1de109fdc0b8d979d4cffeda17
Howard Ding <hding2@illinois.edu>
2013-03-19 15:22:15 -0500

    Fix up the javascript.

--- public/javascripts/google_book.js
+++ public/javascripts/google_book.js
@@ -10,14 +10,14 @@ function get_google_links() {
 }
 
 function get_google_link(span) {
-  var isbn = span.attr('title');
+  var isbn = $jq(span.attr('title'));
   //lookup stuff
   var link_url = 'http://books.google.com/books?id=0JA_uAAACAAJ&dq=isbn:9780470189481&hl=&cd=1&source=gbs_api';
   var image_url = 'http://bks3.books.google.com/books?id=0JA_uAAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api';
   var image = document.createElement('img');
-  image.attr('src', image_url);
+  $jq(image).attr('src', image_url);
   var link = document.createElement('a');
-  link.attr('href', link_url);
-  link.append(image);
-  span.append(link);
+  $jq(link).attr('href', link_url);
+  $jq(link).append(image);
+  $jq(span).append(link);
 }
