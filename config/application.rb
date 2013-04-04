a7271687ccefa15bb39ab46e9317e97963ad69f1
Howard Ding <hding2@illinois.edu>
2013-04-04 16:01:28 -0500

    For reasons that are unfortunatly unclear to me we get problems if observers are active when doing db:migrate. Pinched a bit of code to not start them up when running db:migrate.

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,10 @@ module Bibapp
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running
-    config.active_record.observers = :user_observer, :index_observer, :publications_sweeper
+    # We seem to need to skip observers when running migrations - I'm not sure why
+    unless (File.basename($0) == "rake" && ARGV.include?("db:migrate"))
+      config.active_record.observers = :user_observer, :index_observer, :publications_sweeper
+    end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
