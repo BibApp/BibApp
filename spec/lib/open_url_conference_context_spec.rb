49665ea84ba2c8d9b421ac67bfa7ebf817b8f791
Howard Ding <hding2@illinois.edu>
2013-04-22 17:47:08 -0500

    Fixed some tests.

--- spec/lib/open_url_conference_context_spec.rb
+++ spec/lib/open_url_conference_context_spec.rb
@@ -2,14 +2,14 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "OpenUrlConferenceContext" do
   it "should return a valid ConferencePoster type_uri" do
-      Factory.build(:conference_poster).type_uri.should ==  "http://purl.org/eprint/type/ConferencePoster"
+      FactoryGirl.build(:conference_poster).type_uri.should ==  "http://purl.org/eprint/type/ConferencePoster"
   end
-  
+
   it "should generate a valid openurl context" do
     #attrs = FactoryGirl.attributes_for(:conference_paper)
-    
-    Factory.build_stubbed(:conference_poster) do |poster|
-      ns = [Factory.build_stubbed(:name_string)]      
+
+    FactoryGirl.build_stubbed(:conference_poster) do |poster|
+      ns = [FactoryGirl.build_stubbed(:name_string)]
       poster.name_strings = ns
       assert poster.open_url_context_hash
       assert poster.open_url_context_hash['aulast']
--- spec/models/work_subclasses/conference_poster_spec.rb
+++ spec/models/work_subclasses/conference_poster_spec.rb
@@ -4,5 +4,5 @@ describe ConferencePoster do
 
  it_should_behave_like "a title_primary validating work subclass", ConferencePoster,
                        ['Author', 'Editor'], 'Author',
-                        'Editor', "http ://purl.org/eprint/type/ConferencePoster"
+                        'Editor', "http://purl.org/eprint/type/ConferencePoster"
 end
